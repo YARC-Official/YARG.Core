762e6fc43511884998131f2baa1ee42147af92b2
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-05-14 02:54:01 +0100

    Input types

---
+++ YARG.Core/Input/GameInput.cs
@@ -0,0 +1,16 @@
+﻿namespace YARG.Core.Input
+{
+    public struct GameInput
+    {
+        public GameAction  Action { get; }
+        public ActionPhase Phase  { get; }
+        public double      Time   { get; set; }
+
+        public GameInput(GameAction action, double time, ActionPhase phase)
+        {
+            Action = action;
+            Time = time;
+            Phase = phase;
+        }
+    }
+}
