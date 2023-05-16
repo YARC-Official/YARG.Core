f7bcb4ae3319a21c88028e3b97ca437932fd697b
Nathan <nathanhurst03@gmail.com>
2023-05-16 08:15:44 -0600

    Some miscellaneous things (#3)
    * Add temporary test console app
    
    * Add ignore entries for VS Code
    
    * Add readme

---
+++ TestConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+// Temporary console app for quick and dirty testing
+// Changes to this generally shouldn't be committed, but common test procedures are fine to keep around
+
+Console.WriteLine();
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey(intercept: true);
