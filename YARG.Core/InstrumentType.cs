84feb2c467642c5e9a17fb62c09a7d4a5cd8fe1b
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-05-20 00:56:55 +0100

    instrument and player stuff

--- YARG.Core/InstrumentType.cs
+++ YARG.Core/InstrumentType.cs
@@ -5,9 +5,44 @@
         FiveFretGuitar,
         SixFretGuitar,
         Drums,
+        FiveLaneDrums,
         ProGuitar,
         ProKeys,
         Vocals,
         Dj,
+    }
+
+    public enum Instrument
+    {
+        Guitar,
+        Bass,
+        Rhythm,
+        Keys,
+        GuitarCoop,
+        
+        Drums,
+        ProDrums,
+        
+        ProGuitar,
+        ProBass,
+        ProKeys,
+        
+        Vocals,
+        Harmony,
+
+        SixFretGuitar,
+        SixFretBass,
+        SixFretRhythm,
+
+        Invalid = -1,
+    }
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard,
+        Expert,
+        ExpertPlus
     }
 }
---
+++ YARG.Core/YargProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace YARG.Core
+{
+    public class YargProfile
+    {
+
+        public string Name;
+        
+        public InstrumentType InstrumentType;
+
+        public float NoteSpeed;
+        public float HighwayLength;
+
+        public bool LeftyFlip;
+    }
+}
