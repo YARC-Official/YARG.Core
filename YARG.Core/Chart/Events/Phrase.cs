06094e617e7f620337caf8cb647246d47364b3e1
Nathan <nathanhurst03@gmail.com>
2023-06-16 16:00:57 -0600

    Add phrase and text event types

---
+++ YARG.Core/Chart/Events/Phrase.cs
@@ -0,0 +1,40 @@
+namespace YARG.Core.Chart
+{
+    /// <summary>
+    /// Possible phrase types.
+    /// </summary>
+    public enum PhraseType
+    {
+        // Note modifiers
+        StarPower, // Mainly for visuals, notes are already marked directly as SP
+        TremoloLane, // Guitar strum lanes, single drum rolls
+        TrillLane, // Guitar trill lanes, double drum rolls
+        DrumFill, // Also for visuals
+
+        // Versus modes (face-off and the like)
+        VersusPlayer1,
+        VersusPlayer2,
+
+        // Vocals phrases
+        LyricPhrase,
+        PercussionPhrase,
+
+        // Other events
+        Solo, // Also for visuals
+        BigRockEnding,
+    }
+
+    /// <summary>
+    /// A phrase event that occurs in a chart.
+    /// </summary>
+    public class Phrase : ChartEvent
+    {
+        public PhraseType Type { get; }
+
+        public Phrase(PhraseType type, double time, double timeLength, uint tick, uint tickLength)
+            : base(time, timeLength, tick, tickLength)
+        {
+            Type = type;
+        }
+    }
+}
