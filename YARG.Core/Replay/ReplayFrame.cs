83b468eac307d81f071f3b0c188cfa97b51bc305
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-07-08 01:11:13 +0100

    ReplayFrame class

---
+++ YARG.Core/Replay/ReplayFrame.cs
@@ -0,0 +1,20 @@
+using YARG.Core.Engine;
+using YARG.Core.Input;
+
+namespace YARG.Core.Replay
+{
+    public abstract class ReplayFrame
+    {
+        public int        PlayerId;
+        public string     PlayerName;
+        public Instrument Instrument;
+        public Difficulty Difficulty;
+    }
+
+    public class ReplayFrame<TStats> : ReplayFrame where TStats : BaseStats
+    {
+        public TStats      Stats;
+        public int         InputCount;
+        public GameInput[] Inputs;
+    }
+}
