bc3df29dbe6bf0388fce59cfa08dd92c66db2d8e
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-07-15 21:14:54 +0100

    Add documentation for engine stat variables

--- YARG.Core/Engine/BaseStats.cs
+++ YARG.Core/Engine/BaseStats.cs
@@ -2,20 +2,54 @@
 {
     public abstract class BaseStats
     {
+        /// <summary>
+        /// The accumulated score (e.g from notes hit and sustains)
+        /// </summary>
         public int Score;
+
+        /// <summary>
+        /// The player's current combo (such as 500 note streak)
+        /// </summary>
         public int Combo;
+
+        /// <summary>
+        /// The player's highest combo achieved.
+        /// </summary>
         public int MaxCombo;
 
+        /// <summary>
+        /// The player's current score multiplier (e.g 2x, 3x)
+        /// </summary>
         public int ScoreMultiplier;
 
+        /// <summary>
+        /// Number of notes which have been hit.
+        /// </summary>
         public int NotesHit;
+
+        /// <summary>
+        /// Number of notes which have been missed.
+        /// </summary>
         public int NotesMissed;
 
+        /// <summary>
+        /// Amount of Star Power/Overdrive the player currently has.
+        /// </summary>
         public double StarPowerAmount;
 
+        /// <summary>
+        /// True if the player currently has Star Power/Overdrive active.
+        /// </summary>
         public bool IsStarPowerActive;
 
+        /// <summary>
+        /// Number of Star Power phrases which have been hit.
+        /// </summary>
         public int PhrasesHit;
+
+        /// <summary>
+        /// Number of Star Power phrases which have been missed.
+        /// </summary>
         public int PhrasesMissed;
 
         protected BaseStats()
--- YARG.Core/Engine/Guitar/GuitarStats.cs
+++ YARG.Core/Engine/Guitar/GuitarStats.cs
@@ -2,8 +2,19 @@
 {
     public class GuitarStats : BaseStats
     {
+        /// <summary>
+        /// Number of overstrums which have occurred.
+        /// </summary>
         public int Overstrums;
+
+        /// <summary>
+        /// Number of hammer-ons/pull-offs which have been strummed.
+        /// </summary>
         public int HoposStrummed;
+
+        /// <summary>
+        /// Number of ghost inputs the player has made.
+        /// </summary>
         public int GhostInputs;
 
         public GuitarStats()
