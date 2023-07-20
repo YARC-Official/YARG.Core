d6966f150778c0c52365fc882c16dbaec48adcb4
Nathan <nathanhurst03@gmail.com>
2023-07-20 14:17:53 -0600

    Clean up/fix some song.ini parsing things

--- YARG.Core/Chart/Metadata/SongMetadata.SongIni.cs
+++ YARG.Core/Chart/Metadata/SongMetadata.SongIni.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using EasySharpIni;
 using EasySharpIni.Converters;
 using EasySharpIni.Models;
@@ -9,6 +8,10 @@ namespace YARG.Core.Chart
 {
     public partial class SongMetadata
     {
+        private const string INTEGER_GENERAL_DEFAULT = "-1";
+        private const string INTEGER_TIME_DEFAULT = "-1000";
+        private const string BOOLEAN_GENERAL_DEFAULT = "false";
+
         private static readonly IntConverter IntConverter = new();
         private static readonly BooleanConverter BooleanConverter = new();
 
@@ -41,73 +44,72 @@ namespace YARG.Core.Chart
 
             LoadingPhrase = section.GetField("loading_phrase");
 
-            PlaylistTrack = section.GetField("playlist_track", "-1").Get(IntConverter);
-            AlbumTrack = section.GetField("album_track", "-1").Get(IntConverter);
-            if (AlbumTrack == 0)
+            PlaylistTrack = section.GetField("playlist_track", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
+            AlbumTrack = section.GetField("album_track", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
+            if (AlbumTrack < 1)
             {
                 // Legacy tag
-                AlbumTrack = section.GetField("track", "-1").Get(IntConverter);
+                AlbumTrack = section.GetField("track", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
             }
 
-            SongLength = section.GetField("song_length", "-1").Get(IntConverter);
-            PreviewStart = section.GetField("preview_start_time", "-1000").Get(IntegerTimeConverter);
-            PreviewEnd = section.GetField("preview_end_time", "-1000").Get(IntegerTimeConverter);
+            SongLength = section.GetField("song_length", INTEGER_TIME_DEFAULT).Get(IntegerTimeConverter);
+            PreviewStart = section.GetField("preview_start_time", INTEGER_TIME_DEFAULT).Get(IntegerTimeConverter);
+            PreviewEnd = section.GetField("preview_end_time", INTEGER_TIME_DEFAULT).Get(IntegerTimeConverter);
 
             ChartOffset = section.GetField("delay").Get(DecimalTimeConverter);
 
             VideoStartTime = section.GetField("video_start_time", "0").Get(IntegerTimeConverter);
-            VideoEndTime = section.GetField("video_end_time", "-1").Get(IntegerTimeConverter);
+            VideoEndTime = section.GetField("video_end_time", INTEGER_TIME_DEFAULT).Get(IntegerTimeConverter);
 
-            const string DIFFICULTY_DEFAULT = "-1";
-            BandDifficulty = section.GetField("diff_band", DIFFICULTY_DEFAULT).Get(IntConverter);
+            BandDifficulty = section.GetField("diff_band", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
             PartDifficulties = new()
             {
-                { Instrument.FiveFretGuitar,        section.GetField("diff_guitar", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.FiveFretCoopGuitar,    section.GetField("diff_guitar_coop", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.FiveFretRhythm,        section.GetField("diff_rhythm", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.FiveFretBass,          section.GetField("diff_bass", DIFFICULTY_DEFAULT).Get(IntConverter) },
+                { Instrument.FiveFretGuitar,        section.GetField("diff_guitar", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.FiveFretCoopGuitar,    section.GetField("diff_guitar_coop", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.FiveFretRhythm,        section.GetField("diff_rhythm", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.FiveFretBass,          section.GetField("diff_bass", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
 
                                                     // yes, guitarghl and bassghl have no underscore before "ghl"
-                { Instrument.SixFretGuitar,         section.GetField("diff_guitarghl", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.SixFretCoopGuitar,     section.GetField("diff_guitar_coop_ghl", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.SixFretRhythm,         section.GetField("diff_rhythm_ghl", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.SixFretBass,           section.GetField("diff_bassghl", DIFFICULTY_DEFAULT).Get(IntConverter) },
+                { Instrument.SixFretGuitar,         section.GetField("diff_guitarghl", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.SixFretCoopGuitar,     section.GetField("diff_guitar_coop_ghl", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.SixFretRhythm,         section.GetField("diff_rhythm_ghl", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.SixFretBass,           section.GetField("diff_bassghl", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
 
-                { Instrument.ProGuitar_17Fret,      section.GetField("diff_guitar_real", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.ProGuitar_22Fret,      section.GetField("diff_guitar_real_22", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.ProBass_17Fret,        section.GetField("diff_bass_real", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.ProBass_22Fret,        section.GetField("diff_bass_real_22", DIFFICULTY_DEFAULT).Get(IntConverter) },
+                { Instrument.ProGuitar_17Fret,      section.GetField("diff_guitar_real", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.ProGuitar_22Fret,      section.GetField("diff_guitar_real_22", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.ProBass_17Fret,        section.GetField("diff_bass_real", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.ProBass_22Fret,        section.GetField("diff_bass_real_22", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
 
-                { Instrument.FourLaneDrums,         section.GetField("diff_drums", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.FiveLaneDrums,         section.GetField("diff_drums", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.ProDrums,              section.GetField("diff_drums_real", DIFFICULTY_DEFAULT).Get(IntConverter) },
+                { Instrument.FourLaneDrums,         section.GetField("diff_drums", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.FiveLaneDrums,         section.GetField("diff_drums", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.ProDrums,              section.GetField("diff_drums_real", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
 
-                { Instrument.Keys,                  section.GetField("diff_keys", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                // { Instrument.ProKeys,               section.GetField("diff_keys_real", DIFFICULTY_DEFAULT).Get(IntConverter) },
+                { Instrument.Keys,                  section.GetField("diff_keys", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                // { Instrument.ProKeys,               section.GetField("diff_keys_real", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
 
-                { Instrument.Vocals,                section.GetField("diff_vocals", DIFFICULTY_DEFAULT).Get(IntConverter) },
-                { Instrument.Harmony,               section.GetField("diff_vocals_harm", DIFFICULTY_DEFAULT).Get(IntConverter) },
+                { Instrument.Vocals,                section.GetField("diff_vocals", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
+                { Instrument.Harmony,               section.GetField("diff_vocals_harm", INTEGER_GENERAL_DEFAULT).Get(IntConverter) },
             };
 
             var drumsType = DrumsType.Unknown;
-            if (section.GetField("pro_drums", "false").Get(BooleanConverter))
+            if (section.GetField("pro_drums", BOOLEAN_GENERAL_DEFAULT).Get(BooleanConverter))
             {
                 drumsType = DrumsType.FourLane;
             }
-            else if (section.GetField("five_lane_drums", "false").Get(BooleanConverter))
+            else if (section.GetField("five_lane_drums", BOOLEAN_GENERAL_DEFAULT).Get(BooleanConverter))
             {
                 drumsType = DrumsType.FiveLane;
             }
 
-            int hopoThreshold = section.GetField("hopo_frequency", "-1").Get(IntConverter);
-            bool eighthNoteHopo = section.GetField("eighthnote_hopo", "false").Get(BooleanConverter);
-            int susCutoffThreshold = section.GetField("sustain_cutoff_threshold", "-1").Get(IntConverter);
+            int hopoThreshold = section.GetField("hopo_frequency", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
+            bool eighthNoteHopo = section.GetField("eighthnote_hopo", BOOLEAN_GENERAL_DEFAULT).Get(BooleanConverter);
+            int susCutoffThreshold = section.GetField("sustain_cutoff_threshold", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
 
-            int starPowerNote = section.GetField("multiplier_note", "-1").Get(IntConverter);
+            int starPowerNote = section.GetField("multiplier_note", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
             if (starPowerNote < 0)
             {
                 // Legacy tag from Phase Shift
-                starPowerNote = section.GetField("star_power_note", "-1").Get(IntConverter);
+                starPowerNote = section.GetField("star_power_note", INTEGER_GENERAL_DEFAULT).Get(IntConverter);
             }
 
             ParseSettings = new()
