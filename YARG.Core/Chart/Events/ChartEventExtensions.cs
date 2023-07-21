58f4e9b79c280138ab1f3139c4141e50f6f850a2
Nathan <nathanhurst03@gmail.com>
2023-07-21 01:33:28 -0600

    Ensure `this` parameter names for chart extensions are consistent

--- YARG.Core/Chart/Events/ChartEventExtensions.cs
+++ YARG.Core/Chart/Events/ChartEventExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using YARG.Core.Extensions;
 
@@ -5,145 +6,145 @@ namespace YARG.Core.Chart
 {
     public static class ChartEventExtensions
     {
-        public static double GetStartTime<TEvent>(this IList<TEvent> list)
+        public static double GetStartTime<TEvent>(this IList<TEvent> events)
             where TEvent : ChartEvent
         {
-            if (list.Count < 1)
+            if (events.Count < 1)
                 return 0;
 
             // Chart events are sorted
-            var chartEvent = list[0];
+            var chartEvent = events[0];
             return chartEvent.Time;
         }
 
-        public static double GetEndTime<TEvent>(this IList<TEvent> list)
+        public static double GetEndTime<TEvent>(this IList<TEvent> events)
             where TEvent : ChartEvent
         {
-            if (list.Count < 1)
+            if (events.Count < 1)
                 return 0;
 
             // Chart events are sorted
-            var chartEvent = list[^1];
+            var chartEvent = events[^1];
             return chartEvent.TimeEnd;
         }
 
-        public static uint GetFirstTick<TEvent>(this IList<TEvent> list)
+        public static uint GetFirstTick<TEvent>(this IList<TEvent> events)
             where TEvent : ChartEvent
         {
-            if (list.Count < 1)
+            if (events.Count < 1)
                 return 0;
 
             // Chart events are sorted
-            var chartEvent = list[0];
+            var chartEvent = events[0];
             return chartEvent.Tick;
         }
 
-        public static uint GetLastTick<TEvent>(this IList<TEvent> list)
+        public static uint GetLastTick<TEvent>(this IList<TEvent> events)
             where TEvent : ChartEvent
         {
-            if (list.Count < 1)
+            if (events.Count < 1)
                 return 0;
 
             // Chart events are sorted
-            var chartEvent = list[^1];
+            var chartEvent = events[^1];
             return chartEvent.TickEnd;
         }
 
-        public static TEvent GetPrevious<TEvent>(this IList<TEvent> chartEvents, double time)
+        public static TEvent GetPrevious<TEvent>(this IList<TEvent> events, double time)
             where TEvent : ChartEvent
         {
-            int index = GetIndexOfPrevious(chartEvents, time);
+            int index = GetIndexOfPrevious(events, time);
             if (index < 0)
                 return null;
 
-            return chartEvents[index];
+            return events[index];
         }
 
-        public static TEvent GetPrevious<TEvent>(this IList<TEvent> chartEvents, uint tick)
+        public static TEvent GetPrevious<TEvent>(this IList<TEvent> events, uint tick)
             where TEvent : ChartEvent
         {
-            int index = GetIndexOfPrevious(chartEvents, tick);
+            int index = GetIndexOfPrevious(events, tick);
             if (index < 0)
                 return null;
 
-            return chartEvents[index];
+            return events[index];
         }
 
-        public static TEvent GetNext<TEvent>(this IList<TEvent> chartEvents, double time)
+        public static TEvent GetNext<TEvent>(this IList<TEvent> events, double time)
             where TEvent : ChartEvent
         {
-            int index = GetIndexOfNext(chartEvents, time);
+            int index = GetIndexOfNext(events, time);
             if (index < 0)
                 return null;
 
-            return chartEvents[index];
+            return events[index];
         }
 
-        public static TEvent GetNext<TEvent>(this IList<TEvent> chartEvents, uint tick)
+        public static TEvent GetNext<TEvent>(this IList<TEvent> events, uint tick)
             where TEvent : ChartEvent
         {
-            int index = GetIndexOfNext(chartEvents, tick);
+            int index = GetIndexOfNext(events, tick);
             if (index < 0)
                 return null;
 
-            return chartEvents[index];
+            return events[index];
         }
 
-        public static int GetIndexOfPrevious<TEvent>(this IList<TEvent> chartEvents, double time)
+        public static int GetIndexOfPrevious<TEvent>(this IList<TEvent> events, double time)
             where TEvent : ChartEvent
         {
-            int closestIndex = chartEvents.FindClosestEventIndex(time);
+            int closestIndex = events.FindClosestEventIndex(time);
             if (closestIndex < 0)
                 return -1;
 
             // Ensure the index we return is for an event that occurs before (or at) the given time
-            if (chartEvents[closestIndex].Time <= time)
+            if (events[closestIndex].Time <= time)
                 return closestIndex;
             else
                 return closestIndex - 1;
         }
 
-        public static int GetIndexOfPrevious<TEvent>(this IList<TEvent> chartEvents, uint tick)
+        public static int GetIndexOfPrevious<TEvent>(this IList<TEvent> events, uint tick)
             where TEvent : ChartEvent
         {
-            int closestIndex = chartEvents.FindClosestEventIndex(tick);
+            int closestIndex = events.FindClosestEventIndex(tick);
             if (closestIndex < 0)
                 return -1;
 
             // Ensure the index we return is for an event that occurs before (or at) the given tick
-            if (chartEvents[closestIndex].Tick <= tick)
+            if (events[closestIndex].Tick <= tick)
                 return closestIndex;
             else
                 return closestIndex - 1;
         }
 
-        public static int GetIndexOfNext<TEvent>(this IList<TEvent> chartEvents, double time)
+        public static int GetIndexOfNext<TEvent>(this IList<TEvent> events, double time)
             where TEvent : ChartEvent
         {
-            int closestIndex = chartEvents.FindClosestEventIndex(time);
+            int closestIndex = events.FindClosestEventIndex(time);
             if (closestIndex < 0)
                 return -1;
 
             // Ensure the index we return is for an event that occurs after the given time
-            if (chartEvents[closestIndex].Time > time)
+            if (events[closestIndex].Time > time)
                 return closestIndex;
-            else if (closestIndex < chartEvents.Count - 1)
+            else if (closestIndex < events.Count - 1)
                 return closestIndex + 1;
 
             return -1;
         }
 
-        public static int GetIndexOfNext<TEvent>(this IList<TEvent> chartEvents, uint tick)
+        public static int GetIndexOfNext<TEvent>(this IList<TEvent> events, uint tick)
             where TEvent : ChartEvent
         {
-            int closestIndex = chartEvents.FindClosestEventIndex(tick);
+            int closestIndex = events.FindClosestEventIndex(tick);
             if (closestIndex < 0)
                 return -1;
 
             // Ensure the index we return is for an event that occurs after the given tick
-            if (chartEvents[closestIndex].Tick > tick)
+            if (events[closestIndex].Tick > tick)
                 return closestIndex;
-            else if (closestIndex < chartEvents.Count - 1)
+            else if (closestIndex < events.Count - 1)
                 return closestIndex + 1;
 
             return -1;
