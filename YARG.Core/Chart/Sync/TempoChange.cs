0514a59d6337a648344680a70b75ae6201f1331e
Nathan <nathanhurst03@gmail.com>
2023-07-20 20:48:20 -0600

    Add additional time conversions to TempoChange

--- YARG.Core/Chart/Sync/TempoChange.cs
+++ YARG.Core/Chart/Sync/TempoChange.cs
@@ -2,8 +2,12 @@ namespace YARG.Core.Chart
 {
     public class TempoChange : SyncEvent
     {
+        private const float SECONDS_PER_MINUTE = 60f;
+
         public float BeatsPerMinute { get; }
-        public long MicroSecondsPerBeat => (long) (60 / BeatsPerMinute) * 1000 * 1000;
+        public float SecondsPerBeat => SECONDS_PER_MINUTE / BeatsPerMinute;
+        public long MilliSecondsPerBeat => (long) (SECONDS_PER_MINUTE / BeatsPerMinute * 1000);
+        public long MicroSecondsPerBeat => (long) (SECONDS_PER_MINUTE / BeatsPerMinute * 1000 * 1000);
 
         public TempoChange(float tempo, double time, uint tick) : base(time, tick)
         {
