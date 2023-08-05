a0797af9b0000bbbf03d6a45b15179d565de7470
sonicfind <sonicfind2@gmail.com>
2023-08-05 17:50:51 -0500

    Introduce AbridgedFileInfo type
    Holds the filename and last write time of a given file. This will be used for validation purposes.

---
+++ YARG.Core/Song/AbridgedFileInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace YARG.Core.Song
+{
+    public class AbridgedFileInfo
+    {
+        public readonly string FullName;
+        public readonly DateTime LastWriteTime;
+
+        public AbridgedFileInfo(string file) : this(file, File.GetLastWriteTime(file)) { }
+
+        public AbridgedFileInfo(FileInfo info) : this(info.FullName, info.LastWriteTime) { }
+
+        public AbridgedFileInfo(string fullname, DateTime lastWrite)
+        {
+            FullName = fullname;
+            LastWriteTime = lastWrite;
+        }
+
+        public bool IsStillValid()
+        {
+            FileInfo file = new(FullName);
+            return file.Exists && file.LastWriteTime == LastWriteTime;
+        }
+
+        public static implicit operator AbridgedFileInfo(FileInfo info) => new(info);
+    }
+}
