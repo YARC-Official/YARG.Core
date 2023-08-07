2ef5f3bf21cb0b283d7517883e2c65e4b279be3f
sonicfind <sonicfind2@gmail.com>
2023-08-07 09:27:23 -0500

    Add an unsafe memcpy/memmove static class
    Gonna be used for CON entry hashing calculations

---
+++ YARG.Core/Song/Deserialization/UnsafeMemCopy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace YARG.Core.Song.Deserialization
+{
+    public unsafe static class MemoryManipulation
+    {
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern IntPtr MemCpy(void* dest, void* src, UIntPtr count);
+
+        [DllImport("msvcrt.dll", EntryPoint = "memmove", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        public static extern IntPtr MemMove(void* dest, void* src, UIntPtr count);
+    }
+}
