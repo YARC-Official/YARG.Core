91ab17e2c2abc29c038e686c8b1bee7df7d1dedc
sonicfind <sonicfind2@gmail.com>
2023-08-08 21:18:12 -0500

    Add Tests for Song Directory Scanning

---
+++ YARG.Core.UnitTests/Scanning/SongScanningTests.cs
@@ -0,0 +1,48 @@
+﻿using MoonscraperChartEditor.Song.IO;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YARG.Core.Chart;
+using YARG.Core.Song.Cache;
+
+namespace YARG.Core.UnitTests.Scanning
+{
+    public class SongScanningTests
+    {
+        private List<string> songDirectories = new();
+
+        [SetUp]
+        public void Setup()
+        {
+            Assert.That(songDirectories, Is.Not.Empty, "Add directories to scan for the test");
+        }
+
+        [TestCase]
+        public void FullScan()
+        {
+            YargTrace.AddListener(new YargDebugTraceListener());
+            CacheHandler handler = new(Environment.CurrentDirectory, Environment.CurrentDirectory, true, songDirectories.ToArray());
+            SongCache cache;
+            Assert.DoesNotThrow(() =>
+            {
+                cache = handler.RunScan(false);
+            });
+        }
+
+        [TestCase]
+        public void QuickScan()
+        {
+            YargTrace.AddListener(new YargDebugTraceListener());
+            CacheHandler handler = new(Environment.CurrentDirectory, Environment.CurrentDirectory, false, songDirectories.ToArray());
+
+            SongCache cache;
+            Assert.DoesNotThrow(() =>
+            {
+                cache = handler.RunScan(true);
+            });
+        }
+    }
+}
