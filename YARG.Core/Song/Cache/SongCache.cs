5e5f40943ce49e8b38e4057697dd4ea6ab0bf001
sonicfind <sonicfind2@gmail.com>
2023-08-08 19:47:28 -0500

    Add SongCache type

---
+++ YARG.Core/Song/Cache/SongCache.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YARG.Core.Song.Cache
+{
+    [Serializable]
+    public class SongCache
+    {
+        [NonSerialized]
+        public readonly Dictionary<HashWrapper, List<SongMetadata>> entries = new();
+
+        public readonly TitleCategory titles = new();
+        public readonly YearCategory years = new();
+
+        [NonSerialized]
+        public readonly ArtistAlbumCategory artistAlbums = new();
+        [NonSerialized]
+        public readonly SongLengthCategory songLengths = new();
+
+        public readonly NormalCategory artists = new(SongAttribute.Artist);
+        public readonly NormalCategory albums = new(SongAttribute.Album);
+        public readonly NormalCategory genres = new(SongAttribute.Genre);
+        public readonly NormalCategory charters = new(SongAttribute.Charter);
+        public readonly NormalCategory playlists = new(SongAttribute.Playlist);
+        public readonly NormalCategory sources = new(SongAttribute.Source);
+    }
+}
