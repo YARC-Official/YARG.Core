f2b278475bcfcc502a80f994d5c0503c49fa73b8
sonicfind <sonicfind2@gmail.com>
2023-08-09 20:25:34 -0500

    Use a static integer byte buffer for streamextensions

--- YARG.Core/Song/Deserialization/MiniStreamExtensions.cs
+++ YARG.Core/Song/Deserialization/MiniStreamExtensions.cs
@@ -3,18 +3,24 @@ namespace YARG.Core.Song.Deserialization
 {
     public static class MiniStreamExtensions
     {
+        static readonly object intLock = new();
+        static readonly byte[] integerBuffer = new byte[4];
         public static int ReadInt32LE(this Stream s)
         {
-            byte[] buffer = new byte[4];
-            s.Read(buffer, 0, 4);
-            return buffer[3] << 24 | buffer[2] << 16 | buffer[1] << 8 | buffer[0];
+            lock (intLock)
+            {
+                s.Read(integerBuffer, 0, 4);
+                return integerBuffer[3] << 24 | integerBuffer[2] << 16 | integerBuffer[1] << 8 | integerBuffer[0];
+            }
         }
 
         public static int ReadInt32BE(this Stream s)
         {
-            byte[] buffer = new byte[4];
-            s.Read(buffer, 0, 4);
-            return buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
+            lock (intLock)
+            {
+                s.Read(integerBuffer, 0, 4);
+                return integerBuffer[0] << 24 | integerBuffer[1] << 16 | integerBuffer[2] << 8 | integerBuffer[3];
+            }
         }
 
         public static byte[] ReadBytes(this Stream s, int length)
