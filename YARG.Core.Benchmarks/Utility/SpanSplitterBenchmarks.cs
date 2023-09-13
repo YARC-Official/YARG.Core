15cc545979418e2565288a1a302e8b237479a342
Nathan <nathanhurst03@gmail.com>
2023-09-13 03:22:56 -0600

    Add span splitter benchmarks, and re-organize benchmarks a bit

---
+++ YARG.Core.Benchmarks/Utility/SpanSplitterBenchmarks.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using YARG.Core.UnitTests.Parsing;
+using YARG.Core.Utility;
+
+namespace YARG.Core.Benchmarks
+{
+    public class SpanSplitterBenchmarks
+    {
+        private string searchString;
+
+        [GlobalSetup]
+        public void Initialize()
+        {
+            searchString = ChartParseBehaviorTests.GenerateChartFile();
+        }
+
+        [Benchmark]
+        [Arguments('\n')]
+        [Arguments(' ')]
+        public int Split(char searchChar)
+        {
+            int splitCount = 0;
+            foreach (var _ in searchString.SplitAsSpan(searchChar))
+                splitCount++;
+
+            Debug.Assert(splitCount > 0);
+            return splitCount;
+        }
+
+        [Benchmark]
+        [Arguments('\n')]
+        [Arguments(' ')]
+        public int SplitTrimmed(char searchChar)
+        {
+            int splitCount = 0;
+            foreach (var _ in searchString.SplitTrimmed(searchChar))
+                splitCount++;
+
+            Debug.Assert(splitCount > 0);
+            return splitCount;
+        }
+    }
+}
