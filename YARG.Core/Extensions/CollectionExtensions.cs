dd98550b87579e430bf51cd3e0c12cd379849d98
Nathan <nathanhurst03@gmail.com>
2023-09-13 19:23:33 -0600

    Move a few extensions to YARG.Core

--- YARG.Core/Extensions/CollectionExtensions.cs
+++ YARG.Core/Extensions/CollectionExtensions.cs
@@ -6,7 +6,7 @@ namespace YARG.Core.Extensions
     public static class CollectionExtensions
     {
         /// <summary>
-        /// Duplicates a list and every element inside it, such that the new list is
+        /// Duplicates the list and every element inside it, such that the new list is
         /// entirely independent and shares no references with the original.
         /// </summary>
         public static List<T> Duplicate<T>(this IList<T> list)
@@ -21,6 +21,31 @@ namespace YARG.Core.Extensions
             }
 
             return newlist;
+        }
+
+        /// <summary>
+        /// Shuffles the list using the Fisher-Yates shuffle algorithm, using the given random number generator.
+        /// </summary>
+        /// <remarks>
+        /// https://stackoverflow.com/questions/273313/randomize-a-listt
+        /// </remarks>
+        public static void Shuffle<T>(this IList<T> list, Random random)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(0, n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+
+        /// <summary>
+        /// Picks a random value from the list using the given random number generator.
+        /// </summary>
+        public static T PickRandom<T>(this IList<T> list, Random random)
+        {
+            return list[random.Next(0, list.Count)];
         }
 
         /// <summary>
@@ -79,6 +104,38 @@ namespace YARG.Core.Extensions
             }
 
             return index;
+        }
+
+        /// <summary>
+        /// Attempts to peek at the beginning of the queue.
+        /// </summary>
+        /// <returns>
+        /// The peeked value, if available; otherwise the default value of <typeparamref name="T"/>.
+        /// </returns>
+        public static T PeekOrDefault<T>(this Queue<T> queue)
+        {
+            if (queue.TryPeek(out var o))
+            {
+                return o;
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        /// Attempts to dequeue a value from the queue.
+        /// </summary>
+        /// <returns>
+        /// The peeked value, if available; otherwise the default value of <typeparamref name="T"/>.
+        /// </returns>
+        public static T DequeueOrDefault<T>(this Queue<T> queue)
+        {
+            if (queue.TryDequeue(out var o))
+            {
+                return o;
+            }
+
+            return default;
         }
     }
 }
