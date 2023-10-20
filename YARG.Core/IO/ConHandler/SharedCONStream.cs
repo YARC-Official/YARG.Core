c397275abcf33f5dce440cd74e1237fb4a37eaa1
sonicfind <sonicfind2@gmail.com>
2023-10-19 20:42:18 -0500

    Introduce SharedCONStream type

---
+++ YARG.Core/IO/ConHandler/SharedCONStream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace YARG.Core.IO
+{
+    public class SharedCONStream : IDisposable
+    {
+        public readonly FileStream stream;
+        public readonly object Lock = new();
+
+        public SharedCONStream(FileStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public SharedCONStream(string file)
+        {
+            stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read, 1);
+        }
+
+        public void Dispose()
+        {
+            stream.Dispose();
+        }
+    }
+}
