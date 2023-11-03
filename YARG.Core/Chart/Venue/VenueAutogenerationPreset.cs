0fe223308ecc2bf739e7923d1ac81ca51a8353c6
Raphael Goulart <contact@raphaelgoul.art>
2023-11-03 00:12:07 -0300

    (UNTESTED!) read autogen preset json

--- YARG.Core/Chart/Venue/VenueAutogenerationPreset.cs
+++ YARG.Core/Chart/Venue/VenueAutogenerationPreset.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace YARG.Core.Chart
 {
@@ -20,6 +21,7 @@ namespace YARG.Core.Chart
             DefaultSectionPreset = new AutogenerationSectionPreset();
             DefaultSectionPreset.AllowedLightPresets.Add(LightingType.Default);
             DefaultSectionPreset.AllowedPostProcs.Add(PostProcessingType.Default);
+            SectionPresets = new List<AutogenerationSectionPreset>();
             if (File.Exists(path))
             {
                 ReadPresetFromFile(path);
@@ -32,7 +34,36 @@ namespace YARG.Core.Chart
 
         private void ReadPresetFromFile(string path)
         {
-            // TODO: read preset from file (JSON) fuction
+            // TODO: ACTUALLY TEST IT!! GOTTA SLEEP
+            try
+            {
+                JObject o = JObject.Parse(File.ReadAllText(path));
+                string cameraPacing = (string)o.SelectToken("camera_pacing");
+                CameraPacing = StringToCameraPacing(cameraPacing);
+                bool defaultSectionRead = false;
+
+                foreach (var sectionPreset in (JObject)o.SelectToken("section_presets")) {
+                    AutogenerationSectionPreset value = JObjectToSectionPreset((JObject)sectionPreset.Value);
+                    value.SectionName = sectionPreset.Key;
+                    if (sectionPreset.Key.ToLower().Trim() == "default")
+                    {
+                        DefaultSectionPreset = value;
+                        if (defaultSectionRead)
+                        {
+                            YargTrace.DebugWarning("Multiple default sections found in preset: " + path);
+                        }
+                        defaultSectionRead = true;
+                    }
+                    else
+                    {
+                        SectionPresets.Add(value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                YargTrace.LogException(ex, $"Error while loading auto-gen preset {path}");
+            }
         }
 
         public void GenerateLightingEvents(ref SongChart chart) {
@@ -42,8 +73,84 @@ namespace YARG.Core.Chart
         public void GenerateCameraCutEvents(ref SongChart chart) {
             // TODO: camera cut generator function
         }
+
+        private AutogenerationSectionPreset JObjectToSectionPreset(JObject o) {
+            AutogenerationSectionPreset sectionPreset = new AutogenerationSectionPreset();
+            foreach (var parameter in o)
+            {
+                switch (parameter.Key.ToLower().Trim())
+                {
+                    case "allowed_lightpresets":
+                        List<LightingType> allowedLightPresets = new List<LightingType>();
+                        foreach (string key in (JArray)parameter.Value) {
+                            key = key.Trim();
+                            if (VENUE_LIGHTING_CONVERSION_LOOKUP.TryGetValue(key, out eventData))
+                            {
+                                allowedLightPresets.Add(eventData);
+                            }
+                            else
+                            {
+                                YargTrace.DebugWarning("Invalid light preset: " + key);
+                            }
+                        }
+                        sectionPreset.AllowedLightPresets = allowedLightPresets;
+                        break;
+                    case "allowed_postprocs":
+                        List<LightingType> allowedPostProcs = new List<LightingType>();
+                        foreach (string key in (JArray)parameter.Value) {
+                            key = key.Trim();
+                            if (VENUE_TEXT_CONVERSION_LOOKUP.TryGetValue(key, out eventData) && eventData.type == VenueEvent.Type.PostProcessing)
+                            {
+                                allowedPostProcs.Add(eventData.text);
+                            }
+                            else
+                            {
+                                YargTrace.DebugWarning("Invalid post-proc: " + key);
+                            }
+                        }
+                        sectionPreset.AllowedPostProcs = allowedPostProcs;
+                        break;
+                    case "keyframe_rate":
+                        sectionPreset.KeyframeRate = (int)parameter.Value;
+                        break;
+                    case "lightpreset_blendin":
+                        sectionPreset.LightPresetBlendIn = (int)parameter.Value;
+                        break;
+                    case "postproc_blendin":
+                        sectionPreset.PostProcBlendIn = (int)parameter.Value;
+                        break;
+                    /*case "dircut_at_start":
+                        // TODO: add when we have characters / directed camera cuts
+                        break;*/
+                    case "bonusfx_at_start":
+                        sectionPreset.BonusFxAtStart = (bool)parameter.Value;
+                        break;
+                    case "camera_pacing":
+                        sectionPreset.CameraPacing = StringToCameraPacing((string)parameter.Value);
+                        break;
+                }
+            }
+            return sectionPreset;
+        }
         
-       
+        private CameraPacing StringToCameraPacing(string cameraPacing)
+        {
+            switch (cameraPacing.ToLower().Trim()) {
+                case "minimal": 
+                    return CameraPacing.Minimal;
+                case "slow":
+                    return CameraPacing.Slow;
+                case "medium":
+                    return CameraPacing.Medium;
+                case "fast":
+                    return CameraPacing.Fast;
+                case "crazy":
+                    return CameraPacing.Crazy;
+                default:
+                    YargTrace.DebugWarning("Invalid camera pacing in auto-gen preset: " + cameraPacing);
+                    return CameraPacing.Medium;
+            }
+        }
         
     }
 
