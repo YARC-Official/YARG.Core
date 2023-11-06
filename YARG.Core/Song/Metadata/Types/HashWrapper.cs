698ff4c4c1fb774b94cbe6f9161f9668408111e1
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-11-06 17:09:47 -0500

    Exposed HashWrapper's byte array for use in the score database

--- YARG.Core/Song/Metadata/Types/HashWrapper.cs
+++ YARG.Core/Song/Metadata/Types/HashWrapper.cs
@@ -12,8 +12,24 @@ namespace YARG.Core.Song
     public readonly struct HashWrapper : IComparable<HashWrapper>, IEquatable<HashWrapper>
     {
         public static HashAlgorithm Algorithm => SHA1.Create();
-        public const int HASHSIZEINBYTES = 20;
-        private const int NUMINTEGERS = 5;
+
+        public  const int HASH_SIZE_IN_BYTES = 20;
+        private const int INT_COUNT = 5;
+
+        private readonly byte[] _hash;
+        private readonly int    _hashcode;
+
+        public byte[] HashBytes => _hash;
+
+        public HashWrapper(YARGBinaryReader reader)
+            : this(reader.ReadBytes(HASH_SIZE_IN_BYTES))
+        {
+        }
+
+        public HashWrapper(BinaryReader reader)
+            : this(reader.ReadBytes(HASH_SIZE_IN_BYTES))
+        {
+        }
 
         public static HashWrapper Create(byte[] buffer)
         {
@@ -26,23 +42,18 @@ namespace YARG.Core.Song
             return new HashWrapper(Algorithm.ComputeHash(stream));
         }
 
-        private readonly byte[] _hash;
-        private readonly int _hashcode;
-
-        public HashWrapper(YARGBinaryReader reader) : this (reader.ReadBytes(HASHSIZEINBYTES)) {}
-
-        public HashWrapper(BinaryReader reader) : this(reader.ReadBytes(HASHSIZEINBYTES)) { }
-
         public HashWrapper(byte[] hash)
         {
             _hash = hash;
             _hashcode = 0;
+
             int count = hash.Length / 4;
+
             unsafe
             {
-                fixed(byte* p = hash)
+                fixed (byte* p = hash)
                 {
-                    int* integers = (int*)p;
+                    int* integers = (int*) p;
                     for (int i = 0; i < count; i++)
                     {
                         _hashcode ^= integers[i];
@@ -59,13 +70,14 @@ namespace YARG.Core.Song
         public int CompareTo(HashWrapper other)
         {
             Debug.Assert(_hash.Length == other._hash.Length, "Two incompatible hash types used");
+
             unsafe
             {
                 fixed(byte* p = _hash, p2 = other._hash)
                 {
                     int* integers = (int*) p;
                     int* integers2 = (int*) p2;
-                    for (int i = 0; i < NUMINTEGERS; i++)
+                    for (int i = 0; i < INT_COUNT; i++)
                     {
                         if (integers[i] < integers2[i])
                             return -1;
@@ -74,6 +86,7 @@ namespace YARG.Core.Song
                     }
                 }
             }
+
             return 0;
         }
 
