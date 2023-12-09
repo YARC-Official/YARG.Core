f43cc50734cb0cd10c560cb8084b37142df83620
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-12-09 18:28:51 -0500

    Updated engine preset defaults

--- YARG.Core/Game/Presets/EnginePreset.Defaults.cs
+++ YARG.Core/Game/Presets/EnginePreset.Defaults.cs
@@ -10,12 +10,36 @@ namespace YARG.Core.Game
         {
             FiveFretGuitar =
             {
+                StrumLeniency = 0.07,
+                StrumLeniencySmall = 0.03,
                 AntiGhosting = false,
                 InfiniteFrontEnd = true
             }
         };
 
-        public static EnginePreset Precision = new("Precision", true);
+        public static EnginePreset Precision = new("Precision", true)
+        {
+            FiveFretGuitar =
+            {
+                StrumLeniency = 0.05,
+                StrumLeniencySmall = 0.02,
+                HitWindow =
+                {
+                    MaxWindow = 0.15,
+                    MinWindow = 0.13,
+                    IsDynamic = true,
+                }
+            },
+            Drums =
+            {
+                HitWindow =
+                {
+                    MaxWindow = 0.15,
+                    MinWindow = 0.13,
+                    IsDynamic = true,
+                }
+            }
+        };
 
         public static readonly List<EnginePreset> Defaults = new()
         {
