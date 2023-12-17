fa79c51efd48133308e0ba9f0c4c9f57d10de95c
Jonathan Goyvaerts <jonathan.goyvaerts@gmail.com>
2023-12-17 17:36:15 +0100

    Fix StarPower activating even though the chord note wasn't fully hit (Drums) (#122)

--- YARG.Core/Engine/Drums/DrumsEngine.cs
+++ YARG.Core/Engine/Drums/DrumsEngine.cs
@@ -110,7 +110,7 @@ namespace YARG.Core.Engine.Drums
                 EndSolo();
             }
 
-            if (!activationAutoHit && note.IsStarPowerActivator && EngineStats.CanStarPowerActivate)
+            if (!activationAutoHit && note.IsStarPowerActivator && EngineStats.CanStarPowerActivate && note.ParentOrSelf.WasFullyHit())
             {
                 ActivateStarPower();
             }
