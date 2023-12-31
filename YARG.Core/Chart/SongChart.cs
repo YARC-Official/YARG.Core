ffa0114113c5f5e2be3f8954e591f0b9c75c0ede
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-12-31 17:34:21 -0500

    Forgot to update comment

--- YARG.Core/Chart/SongChart.cs
+++ YARG.Core/Chart/SongChart.cs
@@ -165,7 +165,7 @@ namespace YARG.Core.Chart
                     uint endTick = lastTick / AUTO_GEN_SECTION_COUNT * i;
                     double endTime = SyncTrack.TickToTime(endTick);
 
-                    // "0% to 10%", "10% to 20%", etc.
+                    // "0% - 10%", "10% - 20%", etc.
                     var sectionName =
                         $"{100f / AUTO_GEN_SECTION_COUNT * (i - 1):N0}% - {100f / AUTO_GEN_SECTION_COUNT * i:N0}%";
 
