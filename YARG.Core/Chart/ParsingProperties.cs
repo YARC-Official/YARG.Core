8648b8e88f225328672699bf557c22735c8c49e7
Nathan <nathanhurst03@gmail.com>
2024-01-26 19:56:33 -0700

    Add documentation to ParseSettings

--- YARG.Core/Chart/ParsingProperties.cs
+++ YARG.Core/Chart/ParsingProperties.cs
@@ -19,6 +19,9 @@ namespace YARG.Core.Chart
     /// </summary>
     public class ParseSettings
     {
+        /// <summary>
+        /// The default settings to use for parsing.
+        /// </summary>
         public static ParseSettings Default => new()
         {
             DrumsType = DrumsType.Unknown,
@@ -32,18 +35,72 @@ namespace YARG.Core.Chart
             StarPowerNote = SETTING_DEFAULT,
         };
 
+        /// <summary>
+        /// The value used to indicate a setting should be overwritten with the
+        /// appropriate default value for the chart being parsed.
+        /// </summary>
         public const int SETTING_DEFAULT = -1;
 
+        /// <summary>
+        /// The drums mode to parse the drums track as.
+        /// </summary>
         public DrumsType DrumsType;
 
+        /// <summary>
+        /// The tick distance between notes to use as the HOPO threshold.
+        /// </summary>
+        /// <remarks>
+        /// Uses the <c>hopo_threshold</c> tag from song.ini files.<br/>
+        /// Defaults to a 1/12th note.
+        /// </remarks>
         public long HopoThreshold;
+
+        /// <summary>
+        /// The FoF HOPO threshold setting number to use.
+        /// </summary>
+        /// <remarks>
+        /// Uses the <c>hopofreq</c> tag from song.ini files.<br/>
+        /// 0 -> 1/24th note, 1 -> 1/16th note, 2 -> 1/12th note, 3 -> 1/8th note, 4 -> 1/6th note, 5 -> 1/4th note.
+        /// </remarks>
         public int HopoFreq_FoF;
+
+        /// <summary>
+        /// Set the HOPO threshold to a 1/8th note instead of a 1/12th note.
+        /// </summary>
+        /// <remarks>
+        /// Uses the <c>eighthnote_hopo</c> tag from song.ini files.
+        /// </remarks>
         public bool EighthNoteHopo;
+
+        /// <summary>
+        /// The tick threshold to use for sustain cutoffs.
+        /// </summary>
+        /// <remarks>
+        /// Uses the <c>sustain_cutoff_threshold</c> tag from song.ini files.<br/>
+        /// Defaults to a 1/12th note in .mid, and 0 in .chart.
+        /// </remarks>
         public long SustainCutoffThreshold;
+
+        /// <summary>
+        /// The tick threshold to use for snapping together single notes into chords.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to 10 ticks in .mid, and 0 in .chart.
+        /// </remarks>
         public long NoteSnapThreshold;
 
+        /// <summary>
+        /// The MIDI note to use for Star Power phrases in .mid charts.
+        /// </summary>
+        /// <remarks>
+        /// Uses the <c>multiplier_note</c> and <c>star_power_note</c> tags from song.ini files.<br/>
+        /// Defaults to 116.
+        /// </remarks>
         public int StarPowerNote;
 
+        /// <summary>
+        /// Calculates the HOPO threshold to use from the various HOPO settings.
+        /// </summary>
         public float GetHopoThreshold(float resolution)
         {
             // Prefer in this order:
@@ -58,6 +115,20 @@ namespace YARG.Core.Chart
             else if (EighthNoteHopo)
             {
                 return resolution / 2;
+            }
+            else if (HopoFreq_FoF >= 0)
+            {
+                int denominator = HopoFreq_FoF switch
+                {
+                    0 => 24,
+                    1 => 16,
+                    2 => 12,
+                    3 => 8,
+                    4 => 6,
+                    5 => 4,
+                    _ => throw new NotImplementedException($"Unhandled hopofreq value {HopoFreq_FoF}!")
+                };
+                return (resolution * 4) / denominator;
             }
             else
             {
