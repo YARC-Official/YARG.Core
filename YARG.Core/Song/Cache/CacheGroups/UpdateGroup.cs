9cbbac22f8ee15a01a02bfbd00dcfe1e33e26964
sonicfind <sonicfind2@gmail.com>
2024-02-11 10:09:01 -0600

    Add SongUpdate type that holds all update fileinfo

--- YARG.Core/Song/Cache/CacheGroups/UpdateGroup.cs
+++ YARG.Core/Song/Cache/CacheGroups/UpdateGroup.cs
@@ -1,9 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using YARG.Core.IO;
 
 namespace YARG.Core.Song.Cache
 {
+    public class SongUpdate
+    {
+        public readonly string Name;
+        public readonly AbridgedFileInfo? Midi;
+        public readonly AbridgedFileInfo? Mogg;
+        public readonly AbridgedFileInfo? Milo;
+        public readonly AbridgedFileInfo? Image;
+
+        public SongUpdate(string baseDirectory, string name)
+        {
+            Name = name;
+
+            string dir = Path.Combine(baseDirectory, name);
+            Midi = Get(Path.Combine(dir, name + "_update.mid"));
+            Mogg = Get(Path.Combine(dir, name + "_update.mogg"));
+
+            dir = Path.Combine(dir, "gen");
+            Milo = Get(Path.Combine(dir, name + ".milo_xbox"));
+            Image = Get(Path.Combine(dir, name + "_keep.png_xbox"));
+        }
+
+        private AbridgedFileInfo? Get(string filename)
+        {
+            var imageInfo = new FileInfo(filename);
+            return imageInfo.Exists ? new AbridgedFileInfo(imageInfo, false) : null;
+        }
+    }
+
     public sealed class UpdateGroup : IModificationGroup
     {
         private readonly string _directory;
