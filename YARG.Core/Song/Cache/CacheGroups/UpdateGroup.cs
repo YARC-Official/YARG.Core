5e8234d5fd3b2102515d6cc2f092ea798a2667a1
sonicfind <sonicfind2@gmail.com>
2024-02-12 00:25:02 -0600

    Expand SongUpdate type with extra functionality
    + Allow an instance to hold multiple readers (because that can happen in a single songs_update.dta)
    + Don't internally hold the nodename. The UpdateGroup will use it as the key for a dictionary of updates.
    + Added Serialization and cache reader verification functions.

--- YARG.Core/Song/Cache/CacheGroups/UpdateGroup.cs
+++ YARG.Core/Song/Cache/CacheGroups/UpdateGroup.cs
@@ -1,35 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using YARG.Core.Extensions;
 using YARG.Core.IO;
 
 namespace YARG.Core.Song.Cache
 {
-    public class SongUpdate
+    public class SongUpdate : IComparable<SongUpdate>
     {
-        public readonly string Name;
+        private readonly DateTime _dtaLastWrite;
+        private readonly List<YARGDTAReader> _readers = new();
+
+        public readonly string Directory;
         public readonly AbridgedFileInfo? Midi;
         public readonly AbridgedFileInfo? Mogg;
         public readonly AbridgedFileInfo? Milo;
         public readonly AbridgedFileInfo? Image;
 
-        public SongUpdate(string baseDirectory, string name)
+        public YARGDTAReader[] Readers
         {
-            Name = name;
-
-            string dir = Path.Combine(baseDirectory, name);
-            Midi = Get(Path.Combine(dir, name + "_update.mid"));
-            Mogg = Get(Path.Combine(dir, name + "_update.mogg"));
-
-            dir = Path.Combine(dir, "gen");
-            Milo = Get(Path.Combine(dir, name + ".milo_xbox"));
-            Image = Get(Path.Combine(dir, name + "_keep.png_xbox"));
+            get
+            {
+                var readers = new YARGDTAReader[_readers.Count];
+                for (int i = 0; i < readers.Length; i++)
+                {
+                    readers[i] = _readers[i].Clone();
+                }
+                return readers;
+            }
         }
 
-        private AbridgedFileInfo? Get(string filename)
+        public SongUpdate(string directory, string name, in DateTime dtaLastWrite)
         {
-            var imageInfo = new FileInfo(filename);
-            return imageInfo.Exists ? new AbridgedFileInfo(imageInfo, false) : null;
+            _dtaLastWrite = dtaLastWrite;
+
+            Directory = directory;
+            Midi = GetInfo(Path.Combine(directory, name + "_update.mid"));
+            Mogg = GetInfo(Path.Combine(directory, name + "_update.mogg"));
+
+            directory = Path.Combine(directory, "gen");
+            Milo = GetInfo(Path.Combine(directory, name + ".milo_xbox"));
+            Image = GetInfo(Path.Combine(directory, name + "_keep.png_xbox"));
+
+            static AbridgedFileInfo? GetInfo(string filename)
+            {
+                var info = new FileInfo(filename);
+                return info.Exists ? new AbridgedFileInfo(info, false) : null;
+            }
+        }
+
+        public void AddReader(YARGDTAReader reader)
+        {
+            _readers.Add(reader);
+        }
+
+        public void Serialize(BinaryWriter writer)
+        {
+            WriteInfo(Midi, writer);
+            WriteInfo(Mogg, writer);
+            WriteInfo(Milo, writer);
+            WriteInfo(Image, writer);
+
+            static void WriteInfo(AbridgedFileInfo? info, BinaryWriter writer)
+            {
+                if (info != null)
+                {
+                    writer.Write(true);
+                    writer.Write(info.LastUpdatedTime.ToBinary());
+                }
+                else
+                {
+                    writer.Write(false);
+                }
+            }
+        }
+
+        public bool Validate(BinaryReader reader)
+        {
+            if (!CheckInfo(Midi, reader))
+            {
+                goto FailedMidi;
+            }
+
+            if (!CheckInfo(Mogg, reader))
+            {
+                goto FailedMogg;
+            }
+
+            if (!CheckInfo(Milo, reader))
+            {
+                goto FailedMilo;
+            }
+            return CheckInfo(Image, reader);
+
+        FailedMidi:
+            SkipInfo(reader);
+        FailedMogg:
+            SkipInfo(reader);
+        FailedMilo:
+            SkipInfo(reader);
+            return false;
+
+            static bool CheckInfo(AbridgedFileInfo? info, BinaryReader reader)
+            {
+                if (reader.ReadBoolean())
+                {
+                    var lastWrite = DateTime.FromBinary(reader.ReadInt64());
+                    if (info == null || info.LastUpdatedTime != lastWrite)
+                    {
+                        return false;
+                    }
+                }
+                else if (info != null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public int CompareTo(SongUpdate other)
+        {
+            return _dtaLastWrite.CompareTo(other._dtaLastWrite);
+        }
+
+        public static void SkipInfo(BinaryReader reader)
+        {
+            if (reader.ReadBoolean())
+            {
+                reader.Move(SongMetadata.SIZEOF_DATETIME);
+            }
         }
     }
 
