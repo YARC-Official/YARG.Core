18432bc842fde1b6f0a99fb5d71669fd24fa3b2a
Nathan <nathanhurst03@gmail.com>
2024-02-17 01:11:53 -0700

    Create .chart section handler abstraction

---
+++ YARG.Core/Chart/Parsing/DotChart/DotChartSectionHandler.cs
@@ -0,0 +1,92 @@
+using System;
+using YARG.Core.Logging;
+using YARG.Core.Utility;
+
+namespace YARG.Core.Chart.Parsing
+{
+    using AsciiTrimSplitter = SpanSplitter<char, AsciiTrimSplitProcessor>;
+
+    internal abstract class DotChartSectionHandler
+    {
+        protected readonly SongChart _chart;
+
+        public DotChartSectionHandler(SongChart chart)
+        {
+            _chart = chart;
+        }
+
+        protected abstract void FinishTick(uint tick);
+
+        protected abstract bool ProcessEvent(ReadOnlySpan<char> typeText, ReadOnlySpan<char> eventText);
+
+        public void ParseSection(AsciiTrimSplitter section)
+        {
+            uint currentTick = 0;
+            foreach (var line in section)
+            {
+                // 1234 = TS 4 3
+
+                // Parse tick
+                var tickText = line.SplitOnceTrimmedAscii('=', out var eventText);
+                if (!uint.TryParse(tickText, out uint tick))
+                {
+                    YargLogger.LogFormatError("Failed to parse tick text: {0}", tickText.ToString());
+                    continue;
+                }
+
+                // Split event type and data
+                var typeText = eventText.SplitOnceTrimmedAscii(' ', out eventText);
+                if (typeText.IsEmpty || eventText.IsEmpty)
+                {
+                    YargLogger.LogFormatError("Malformed .chart event: {0}", line.ToString());
+                    continue;
+                }
+
+                if (tick != currentTick)
+                {
+                    if (tick < currentTick)
+                        throw new Exception($"Tick cannot go backwards! Went from {currentTick} to {tick}");
+
+                    FinishTick(currentTick);
+                    currentTick = tick;
+                }
+
+                if (!ProcessEvent(typeText, eventText))
+                {
+                    // Only warn on unknown data in debug mode
+                    YargLogger.LogFormatDebug("Unknown .chart event: {0}", line.ToString());
+                }
+            }
+
+            FinishTick(currentTick);
+        }
+
+        protected static uint ReadEventInt32(ReadOnlySpan<char> text)
+        {
+            return uint.TryParse(text, out uint value)
+                ? value
+                : throw new Exception($"Failed to parse uint text: {text.ToString()}");
+        }
+
+        protected static ulong ReadEventInt64(ReadOnlySpan<char> text)
+        {
+            return ulong.TryParse(text, out ulong value)
+                ? value
+                : throw new Exception($"Failed to parse ulong text: {text.ToString()}");
+        }
+
+        protected static void ReadEventInt32Pair(ReadOnlySpan<char> text, out uint param1, out uint param2)
+        {
+            var param1Text = text.SplitOnceTrimmedAscii(' ', out var param2Text);
+            param1 = ReadEventInt32(param1Text);
+            param2 = ReadEventInt32(param2Text);
+        }
+
+        protected static void ReadEventInt32Pair(ReadOnlySpan<char> text, out uint param1, out uint? param2)
+        {
+            var param1Text = text.SplitOnceTrimmedAscii(' ', out var param2Text);
+            param1 = ReadEventInt32(param1Text);
+            param2 = param2Text.IsEmpty ? null : ReadEventInt32(param2Text);
+        }
+    }
+}
---
+++ YARG.Core/Chart/Parsing/DotChart/Handlers/DotChartSyncTrackHandler.cs
@@ -0,0 +1,89 @@
+using System;
+
+namespace YARG.Core.Chart.Parsing
+{
+    internal sealed class DotChartSyncTrackHandler : DotChartSectionHandler
+    {
+        private bool _hasTempo;
+        private bool _hasTimesig;
+
+        private float _tempo;
+
+        private uint _numerator;
+        private uint _denominator;
+
+        public DotChartSyncTrackHandler(SongChart chart, uint resolution)
+            : base(chart)
+        {
+            _chart.SyncTrack = new(resolution);
+        }
+
+        protected override void FinishTick(uint tick)
+        {
+            var syncTrack = _chart.SyncTrack;
+
+            double time = syncTrack.Tempos.Count > 0
+                ? syncTrack.TickToTime(tick, syncTrack.Tempos[^1])
+                : 0;
+
+            if (_hasTempo)
+                syncTrack.Tempos.Add(new(_tempo, time, tick));
+            else if (syncTrack.Tempos.Count < 1)
+                syncTrack.Tempos.Add(new(120, 0, 0));
+
+            if (_hasTimesig)
+                syncTrack.TimeSignatures.Add(new(_numerator, _denominator, time, tick));
+            else if (syncTrack.TimeSignatures.Count < 1)
+                syncTrack.TimeSignatures.Add(new(4, 4, 0, 0));
+
+            _hasTempo = false;
+            _hasTimesig = false;
+        }
+
+        protected override bool ProcessEvent(ReadOnlySpan<char> typeText, ReadOnlySpan<char> eventText)
+        {
+            if (typeText.Equals("TS", StringComparison.OrdinalIgnoreCase))
+            {
+                ReadEventInt32Pair(eventText, out uint numerator, out uint? denominatorPower);
+                uint denominator = Pow(2, denominatorPower ?? 2);
+
+                _hasTimesig = true;
+                _numerator = numerator;
+                _denominator = denominator;
+
+                return true;
+            }
+            else if (typeText.Equals("B", StringComparison.OrdinalIgnoreCase))
+            {
+                float tempo = ReadEventInt32(eventText) / 1000f;
+
+                _hasTempo = true;
+                _tempo = tempo;
+
+                return true;
+            }
+            else if (typeText.Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                // Not necessary currently, but we'll want these for YACE
+                // ulong microseconds = ReadEventInt64(eventText);
+                return true;
+            }
+
+            return false;
+        }
+
+        private static uint Pow(uint x, uint y)
+        {
+            if (y == 0)
+                return 1;
+
+            while (y > 1)
+            {
+                checked { x *= x; }
+                y--;
+            }
+
+            return x;
+        }
+    }
+}
