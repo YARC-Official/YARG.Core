28174da43d18438f8bea489cafe9fd8d0ae173ba
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-03-13 13:40:40 +0000

    Make LogException message optional parameter

--- YARG.Core/Logging/YargLogger.Logging.cs
+++ YARG.Core/Logging/YargLogger.Logging.cs
@@ -5,7 +5,7 @@ namespace YARG.Core.Logging
 {
     public static partial class YargLogger
     {
-        public static void LogException(Exception ex, string? message, [CallerFilePath] string source = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "")
+        public static void LogException(Exception ex, string? message = "", [CallerFilePath] string source = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "")
         {
             LogItem logItem = !string.IsNullOrEmpty(message)
                 ? FormatLogItem.MakeItem("{0}\n{1}", message, ex)
