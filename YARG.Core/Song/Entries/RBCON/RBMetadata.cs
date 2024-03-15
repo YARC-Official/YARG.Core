a68e100ab637b8cac1b2909f5331b17caf8b3438
sonicfind <sonicfind2@gmail.com>
2024-03-15 13:34:51 -0500

    Shift around RBCON audio handling
    + Update Cache Version

---
+++ YARG.Core/Song/Entries/RBCON/RBMetadata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.Song
+{
+    public struct RBMetadata
+    {
+        public static readonly RBMetadata Default = new()
+        {
+            SongID = string.Empty,
+            DrumBank = string.Empty,
+            VocalPercussionBank = string.Empty,
+            VocalGender = true
+        };
+
+        public string SongID;
+        public uint AnimTempo;
+        public string DrumBank;
+        public string VocalPercussionBank;
+        public uint VocalSongScrollSpeed;
+        public bool VocalGender; //true for male, false for female
+        //public bool HasAlbumArt;
+        //public bool IsFake;
+        public uint VocalTonicNote;
+        public bool SongTonality; // 0 = major, 1 = minor
+        public int TuningOffsetCents;
+        public uint VenueVersion;
+
+        public string[]? Soloes;
+        public string[]? VideoVenues;
+
+        public int[]? RealGuitarTuning;
+        public int[]? RealBassTuning;
+
+        public RBAudio<int> Indices;
+        public RBAudio<float> Panning;
+    }
+}
