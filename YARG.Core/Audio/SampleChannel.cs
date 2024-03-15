c987f28ed05b3618f704620d3162f0725a1414cf
sonicfind <sonicfind2@gmail.com>
2024-03-15 11:46:41 -0500

    Add new base Audio classes

---
+++ YARG.Core/Audio/SampleChannel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.Audio
+{
+    public abstract class SampleChannel : IDisposable
+    {
+        protected const double PLAYBACK_SUPPRESS_THRESHOLD = 0.05f;
+        private bool disposedValue;
+
+        protected readonly string _path;
+        protected readonly int _playbackCount;
+
+        public readonly SfxSample Sample;
+        protected SampleChannel(SfxSample sample, string path, int playbackCount)
+        {
+            Sample = sample;
+            _path = path;
+            _playbackCount = playbackCount;
+        }
+
+        protected virtual void DisposeManagedResources() { }
+        protected virtual void DisposeUnmanagedResources() { }
+
+        public abstract void Play();
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    DisposeManagedResources();
+                }
+                DisposeUnmanagedResources();
+                disposedValue = true;
+            }
+        }
+
+        ~SampleChannel()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ YARG.Core/Audio/StemMixer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YARG.Core.Audio
+{
+    public abstract class StemMixer : IDisposable
+    {
+        private bool _disposed;
+
+        protected readonly AudioManager _manager;
+        protected readonly List<StemChannel> _channels = new();
+
+        protected float _speed;
+        protected bool _isPlaying = false;
+
+        public IReadOnlyList<StemChannel> Channels => _channels;
+
+        public StemChannel? LeadChannel { get; protected set; }
+
+        public bool IsPlaying
+        {
+            get
+            {
+                return _isPlaying && LeadChannel!.GetPosition() < LeadChannel.LengthD;
+            }
+        }
+
+        public abstract event Action SongEnd;
+
+        protected StemMixer(AudioManager manager, float speed)
+        {
+            _manager = manager;
+            _speed = speed;
+
+            _manager.AddMixer(this);
+        }
+
+        public StemChannel? this[SongStem stem] => _channels.Find(x => x.Stem == stem);
+
+        public abstract int Play(bool restart = false);
+        public abstract void FadeIn(float maxVolume);
+        public abstract Task FadeOut(CancellationToken token = default);
+        public abstract int Pause();
+        public abstract double GetPosition(bool bufferCompensation = true);
+        public abstract void SetPosition(double position, bool bufferCompensation = true);
+        public abstract void SetVolume(double volume);
+        public abstract int GetData(float[] buffer);
+        public abstract void SetPlayVolume(bool fadeIn);
+        public abstract void SetSpeed(float speed);
+        public abstract bool AddChannel(SongStem stem);
+        public abstract bool AddChannel(SongStem stem, Stream stream);
+        public abstract bool AddChannel(SongStem stem, int[] indices, float[] panning);
+        public abstract bool RemoveChannel(SongStem stemToRemove);
+
+        protected virtual void DisposeManagedResources() { }
+        protected virtual void DisposeUnmanagedResources() { }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                Pause();
+                if (disposing)
+                {
+                    DisposeManagedResources();
+                }
+                DisposeUnmanagedResources();
+                _disposed = true;
+            }
+        }
+
+        ~StemMixer()
+        {
+            _manager.RemoveMixer(this);
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+        }
+    }
+}
