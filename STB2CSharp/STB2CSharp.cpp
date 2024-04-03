120de2afc7a8a1d46fd5f673c481402822958b48
sonicfind <sonicfind2@gmail.com>
2024-04-03 13:43:02 -0500

    Add std-image wrapper project to solution

---
+++ STB2CSharp/STB2CSharp.cpp
@@ -0,0 +1,12 @@
+﻿#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+extern "C" __declspec(dllexport) unsigned char* load_image_from_memory(const unsigned char* bytes, int length, int* width, int* height, int* components)
+{
+    return stbi_load_from_memory(bytes, length, width, height, components, 0);
+}
+
+extern "C" __declspec(dllexport) void free_image(unsigned char* image)
+{
+    stbi_image_free(image);
+}
