484bab82375ad0fa33a361271bf67e73bd026929
sonicfind <sonicfind2@gmail.com>
2024-04-08 21:23:33 -0500

    Convert StemSettings to linear scaling

--- YARG.Core/Audio/StemSettings.cs
+++ YARG.Core/Audio/StemSettings.cs
@@ -4,19 +4,16 @@ namespace YARG.Core.Audio
 {
     public class StemSettings
     {
-        private const double BASE = 2;
-        private const double FACTOR = BASE - 1;
         private readonly double _volumeScaling;
 
         private Action<double>? _onVolumeChange;
         private Action<bool>? _onReverbChange;
         private double _volume;
-        private double _trueVolume;
         private bool _reverb;
 
         public StemSettings(double scaling)
         {
-            _trueVolume = _volumeScaling = scaling;
+            _volumeScaling = scaling;
             _volume = 1;
         }
 
@@ -38,13 +35,11 @@ namespace YARG.Core.Audio
             set
             {
                 _volume = Math.Clamp(value, 0, 1);
-                double scaled = _volume * _volumeScaling;
-                _trueVolume = (Math.Pow(BASE, scaled) - 1) / FACTOR;
-                _onVolumeChange?.Invoke(_trueVolume);
+                _onVolumeChange?.Invoke(TrueVolume);
             }
         }
 
-        public double TrueVolume => _trueVolume;
+        public double TrueVolume => _volume * _volumeScaling;
 
         public bool Reverb
         {
