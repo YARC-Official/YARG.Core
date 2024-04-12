6704afe40fe655bd46e4d79bcf7d5207c34747de
sonicfind <sonicfind2@gmail.com>
2024-04-12 09:48:32 -0500

    Utilize header file + updated cross platform macros

--- STB2CSharp/STB2CSharp.cpp
+++ STB2CSharp/STB2CSharp.cpp
@@ -1,12 +1,11 @@
-﻿#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+﻿#include "STB2CSharp.h"
 
-extern "C" __declspec(dllexport) unsigned char* load_image_from_memory(const unsigned char* bytes, int length, int* width, int* height, int* components)
+unsigned char* load_image_from_memory(const unsigned char* bytes, int length, int* width, int* height, int* components)
 {
     return stbi_load_from_memory(bytes, length, width, height, components, 0);
 }
 
-extern "C" __declspec(dllexport) void free_image(unsigned char* image)
+void free_image(unsigned char* image)
 {
     stbi_image_free(image);
 }
---
+++ STB2CSharp/STB2CSharp.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#if defined STB_EXPORTS
+    #if defined _WIN32
+        #define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
+    #else
+        #define LIB_API(RetType) extern "C" RetType __attribute__((visibility("default")))
+    #endif
+#else
+    #if defined _WIN32
+        #define LIB_API(RetType) extern "C" __declspec(dllimport) RetType
+    #else
+        #define LIB_API(RetType) extern "C" RetType
+    #endif
+#endif
+
+LIB_API(unsigned char*) load_image_from_memory(const unsigned char* bytes, int length, int* width, int* height, int* components);
+
+LIB_API(void) free_image(unsigned char* image);
