56bd76762187bd509edb201790eb714b62e9669c
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-05-06 16:26:20 -0400

    `YargProKeysEngine`

---
+++ YARG.Core/Engine/ProKeys/Engines/YargProKeysEngine.cs
@@ -0,0 +1,47 @@
+﻿using YARG.Core.Chart;
+using YARG.Core.Input;
+
+namespace YARG.Core.Engine.ProKeys.Engines
+{
+    public class YargProKeysEngine : ProKeysEngine
+    {
+        public YargProKeysEngine(InstrumentDifficulty<GuitarNote> chart, SyncTrack syncTrack, ProKeysEngineParameters engineParameters, bool isBot) : base(chart, syncTrack, engineParameters, isBot)
+        {
+        }
+
+        protected override void UpdateBot(double time)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void MutateStateWithInput(GameInput gameInput)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateHitLogic(double time)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void CheckForNoteHit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool CanNoteBeHit(GuitarNote note)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void AddScore(GuitarNote note)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override int CalculateBaseScore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
