c696753105401112001c680d20f172e1fb993acc
sonicfind <sonicfind2@gmail.com>
2024-05-08 00:21:21 -0500

    Fix Sixfret scanning error
    Open notes baby

--- YARG.Core/Song/Preparsers/Midi/MidiSixFretPreparser.cs
+++ YARG.Core/Song/Preparsers/Midi/MidiSixFretPreparser.cs
@@ -5,6 +5,8 @@ namespace YARG.Core.Song
     public class Midi_SixFret_Preparser : MidiInstrument_Common
     {
         // Open note included
+        private const int NOTE_MIN = 58;
+        private const int NOTE_MAX = 103;
         private const int NUM_LANES = 7;
         private static readonly int[] LANEINDICES = new int[NUM_DIFFICULTIES * NOTES_PER_DIFFICULTY] {
             0, 4, 5, 6, 1, 2, 3, 7, 8, 9, 10, 11,
@@ -24,11 +26,11 @@ namespace YARG.Core.Song
             return preparser.validations;
         }
 
-        protected override bool IsNote() { return 58 <= note.value && note.value <= 103; }
+        protected override bool IsNote() { return NOTE_MIN <= note.value && note.value <= NOTE_MAX; }
 
         protected override bool ParseLaneColor_ON(YARGMidiTrack track)
         {
-            int noteValue = note.value - 58;
+            int noteValue = note.value - NOTE_MIN;
             int diffIndex = DIFFVALUES[noteValue];
             if (!difficultyTracker[diffIndex])
             {
@@ -41,7 +43,7 @@ namespace YARG.Core.Song
 
         protected override bool ParseLaneColor_Off(YARGMidiTrack track)
         {
-            int noteValue = note.value - 59;
+            int noteValue = note.value - NOTE_MIN;
             int diffIndex = DIFFVALUES[noteValue];
             if (!difficultyTracker[diffIndex])
             {
