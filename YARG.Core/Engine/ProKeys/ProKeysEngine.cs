2979a34bb1b3ad89f7502e715f4da92a6396ca15
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-05-14 17:10:18 -0400

    Got started on some pro-keys engine stuff

--- YARG.Core/Engine/ProKeys/ProKeysEngine.cs
+++ YARG.Core/Engine/ProKeys/ProKeysEngine.cs
@@ -1,14 +1,63 @@
-﻿using YARG.Core.Chart;
+﻿using System;
+using YARG.Core.Chart;
+using YARG.Core.Logging;
 
 namespace YARG.Core.Engine.ProKeys
 {
     public abstract class ProKeysEngine : BaseEngine<ProKeysNote, ProKeysEngineParameters,
         ProKeysStats, ProKeysEngineState>
     {
+        public Action? OnOverhit;
+
         protected ProKeysEngine(InstrumentDifficulty<ProKeysNote> chart, SyncTrack syncTrack,
             ProKeysEngineParameters engineParameters, bool isBot)
             : base(chart, syncTrack, engineParameters, false, isBot)
         {
+        }
+
+        public override void Reset(bool keepCurrentButtons = false)
+        {
+            var keys = State.KeyMask;
+
+            base.Reset(keepCurrentButtons);
+
+            if (keepCurrentButtons)
+            {
+                State.KeyMask = keys;
+            }
+        }
+
+        protected virtual void Overhit()
+        {
+            // Can't overstrum before first note is hit/missed
+            if (State.NoteIndex == 0)
+            {
+                return;
+            }
+
+            // Cancel overstrum if past last note and no active sustains
+            if (State.NoteIndex >= Chart.Notes.Count /*&& ActiveSustains.Count == 0*/)
+            {
+                return;
+            }
+
+            YargLogger.LogFormatTrace("Overhit at {0}", State.CurrentTime);
+
+            if (State.NoteIndex < Notes.Count)
+            {
+                // Don't remove the phrase if the current note being overstrummed is the start of a phrase
+                if (!Notes[State.NoteIndex].IsStarPowerStart)
+                {
+                    StripStarPower(Notes[State.NoteIndex]);
+                }
+            }
+
+            EngineStats.Combo = 0;
+            EngineStats.Overhits++;
+
+            UpdateMultiplier();
+
+            OnOverhit?.Invoke();
         }
     }
 }
--- YARG.Core/Engine/ProKeys/ProKeysEngineState.cs
+++ YARG.Core/Engine/ProKeys/ProKeysEngineState.cs
@@ -2,6 +2,25 @@
 {
     public class ProKeysEngineState : BaseEngineState
     {
+        public int KeyMask;
 
+        /// <summary>
+        /// The integer value for the key that was hit this update. <c>null</c> is none.
+        /// </summary>
+        public int? KeyHit;
+        /// <summary>
+        /// The integer value for the key that was released this update. <c>null</c> is none.
+        /// </summary>
+        public int? KeyReleased;
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            KeyMask = 0;
+
+            KeyHit = null;
+            KeyReleased = null;
+        }
     }
 }
