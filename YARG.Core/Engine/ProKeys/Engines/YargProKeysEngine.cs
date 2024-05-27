d1a9913e9740665d6b16e9ff9de35c8aafb2cc39
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-05-27 15:53:03 +0100

    Fat Finger logic (does not yet work with chords)

--- YARG.Core/Engine/ProKeys/Engines/YargProKeysEngine.cs
+++ YARG.Core/Engine/ProKeys/Engines/YargProKeysEngine.cs
@@ -57,6 +57,44 @@ namespace YARG.Core.Engine.ProKeys.Engines
             // Update bot (will return if not enabled)
             UpdateBot(time);
 
+            if (State.FatFingerTimer.IsActive)
+            {
+                // Fat Fingered key was released before the timer expired
+                if (State.KeyReleased == State.FatFingerKey && !State.FatFingerTimer.IsExpired(State.CurrentTime))
+                {
+                    YargLogger.LogFormatDebug("Released fat fingered key at {0}. Note was hit: {1}", State.CurrentTime, State.FatFingerNote!.WasHit);
+
+                    // The note must be hit to disable the timer
+                    if (State.FatFingerNote!.WasHit)
+                    {
+                        YargLogger.LogDebug("Disabling fat finger timer as the note has been hit.");
+                        State.FatFingerTimer.Disable();
+                        State.FatFingerKey = null;
+                        State.FatFingerNote = null;
+                    }
+                }
+                else if(State.FatFingerTimer.IsExpired(State.CurrentTime))
+                {
+                    YargLogger.LogFormatDebug("Fat Finger timer expired at {0}", State.CurrentTime);
+
+                    var fatFingerKeyMask = 1 << State.FatFingerKey;
+
+                    var isHoldingWrongKey = (State.KeyMask & fatFingerKeyMask) == fatFingerKeyMask;
+
+                    // Overhit if key is still held OR if key is not held but note was not hit either
+                    if (isHoldingWrongKey || (!isHoldingWrongKey && !State.FatFingerNote!.WasHit))
+                    {
+                        YargLogger.LogFormatDebug("Overhit due to fat finger with key {0}. KeyMask: {1}. Holding: {2}. WasHit: {3}",
+                            State.FatFingerKey, State.KeyMask, isHoldingWrongKey, State.FatFingerNote!.WasHit);
+                        Overhit(State.FatFingerKey!.Value);
+                    }
+
+                    State.FatFingerTimer.Disable();
+                    State.FatFingerKey = null;
+                    State.FatFingerNote = null;
+                }
+            }
+
             // Quit early if there are no notes left
             if (State.NoteIndex >= Notes.Count)
             {
@@ -129,6 +167,7 @@ namespace YARG.Core.Engine.ProKeys.Engines
                         {
                             foreach (var note in parentNote.ChordEnumerator())
                             {
+                                // Go to next note if the key hit does not match the note's key
                                 if (State.KeyHit != note.Key)
                                 {
                                     continue;
@@ -137,7 +176,8 @@ namespace YARG.Core.Engine.ProKeys.Engines
                                 if (!State.ChordStaggerTimer.IsActive)
                                 {
                                     StartTimer(ref State.ChordStaggerTimer, State.CurrentTime);
-                                    YargLogger.LogFormatDebug("Starting chord staggering at {0}. End time is {1}", State.CurrentTime, State.ChordStaggerTimer.EndTime);
+                                    YargLogger.LogFormatDebug("Starting chord staggering at {0}. End time is {1}",
+                                        State.CurrentTime, State.ChordStaggerTimer.EndTime);
 
                                     var chordStaggerEndTime = State.ChordStaggerTimer.EndTime;
 
@@ -170,7 +210,32 @@ namespace YARG.Core.Engine.ProKeys.Engines
             // If no note was hit but the user hit a key, then over hit
             if (State.KeyHit != null)
             {
-                Overhit(State.KeyHit.Value);
+                var inWindow = IsNoteInWindow(parentNote);
+                var isAdjacent = ProKeysUtilities.IsAdjacentKey(parentNote.Key, State.KeyHit.Value);
+                var isFatFingerActive = State.FatFingerTimer.IsActive;
+
+                if (!inWindow || !isAdjacent || isFatFingerActive)
+                {
+                    Overhit(State.KeyHit.Value);
+
+                    // TODO Maybe don't disable the timer/use a flag saying no more fat fingers allowed for the current note.
+
+                    State.FatFingerTimer.Disable();
+                    State.FatFingerKey = null;
+                    State.FatFingerNote = null;
+                }
+                else
+                {
+                    StartTimer(ref State.FatFingerTimer, State.CurrentTime);
+                    State.FatFingerKey = State.KeyHit.Value;
+
+                    // TODO Make this work with chords (check for child notes)
+                    State.FatFingerNote = parentNote;
+
+                    YargLogger.LogFormatDebug("Hit adjacent key {0} Starting fat finger timer at {1}. End time: {2}. Key is {3}", State.FatFingerKey, State.CurrentTime,
+                        State.FatFingerTimer.EndTime, State.FatFingerKey);
+                }
+
                 State.KeyHit = null;
             }
         }
