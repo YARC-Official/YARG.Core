9e9eaebe34511e47d2aa87d08ff886771506a3fe
sonicfind <sonicfind2@gmail.com>
2024-05-29 08:48:03 -0500

    Update macros

--- STB2CSharp/STB2CSharp.h
+++ STB2CSharp/STB2CSharp.h
@@ -2,18 +2,24 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#if defined STB_EXPORTS
-    #if defined _WIN32
-        #define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
+#if defined _WIN32 || defined __CYGWIN__
+    #ifdef STB_EXPORTS
+        #ifdef __GNUC__
+            #define LIB_API(RetType) extern "C" __attribute__ ((dllexport)) RetType
+        #else
+            #define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
+        #endif
     #else
-        #define LIB_API(RetType) extern "C" RetType __attribute__((visibility("default")))
+        #ifdef __GNUC__
+            #define LIB_API(RetType) extern "C" __attribute__ ((dllimport)) RetType
+        #else
+            #define LIB_API(RetType) extern "C" __declspec(dllimport) RetType
+        #endif
     #endif
+#elif __GNUC__ >= 4
+    #define LIB_API(RetType) extern "C" __attribute__ ((visibility ("default"))) RetType
 #else
-    #if defined _WIN32
-        #define LIB_API(RetType) extern "C" __declspec(dllimport) RetType
-    #else
-        #define LIB_API(RetType) extern "C" RetType
-    #endif
+    #define LIB_API(RetType) extern "C" RetType
 #endif
 
 LIB_API(unsigned char*) load_image_from_memory(const unsigned char* bytes, int length, int* width, int* height, int* components);
