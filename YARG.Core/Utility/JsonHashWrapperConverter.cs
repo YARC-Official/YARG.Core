26e086dfbc31a67a18825b8cdf47183ba85f5e55
sonicfind <sonicfind2@gmail.com>
2024-06-03 21:57:51 -0500

    Localize HashWrapper Inner Values Within The Structure (#176)
    * Utilize unsafe fixed buffer in Hashwrapper over FixedArray
    
    * Hold hashes by integer vs by bytes
    
    * Remove _hashcode variable
    
    * Re-apply `readonly` modifiers to HashWrapper methods
    
    * Remove `fixed` calls on the hash
    
    * Forgot readonly on HashByte property
    
    * Replace Create() byte[] param with ReadOnlySpan<byte>
    
    + Use stackalloc span in json converter
    
    * Move HashWrapper conversion from string to static function
    
    * Use `Span.CopyTo()` in `HashWrapper.Create()`
    
    * Fix FromString using incorrect conversion type

--- YARG.Core/Utility/JsonHashWrapperConverter.cs
+++ YARG.Core/Utility/JsonHashWrapperConverter.cs
@@ -15,28 +15,7 @@ namespace YARG.Core.Utility
         public override HashWrapper ReadJson(JsonReader reader, Type objectType, HashWrapper existingValue,
             bool hasExistingValue, JsonSerializer serializer)
         {
-            if (reader.Value == null)
-            {
-                return new HashWrapper();
-            }
-
-            try
-            {
-                var hashString = reader.Value.ToString().AsSpan();
-                var hashBytes = new byte[HashWrapper.HASH_SIZE_IN_BYTES];
-
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    var b = byte.Parse(hashString.Slice(i * 2, 2), NumberStyles.AllowHexSpecifier);
-                    hashBytes[i] = b;
-                }
-
-                return new HashWrapper(hashBytes);
-            }
-            catch
-            {
-                return new HashWrapper();
-            }
+            return reader.Value != null ? HashWrapper.FromString(reader.Value.ToString().AsSpan()) : default;
         }
     }
 }
