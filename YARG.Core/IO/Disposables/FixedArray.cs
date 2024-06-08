61daa956eafe6102dc96393836c3e83202f3ddea
sonicfind <sonicfind2@gmail.com>
2024-06-08 17:30:35 -0500

    Convert FixedArray into a small hierarchy of fixed array types [Breaking change]
    All types contains pointers and lengths. What differs is how the memory is allocated. One is the new memorymappedArray which uses the backing of a memory mapped file while the other is for new empty allocations.
    
    Note: FixedArray's base class will function simply as an alias. It simply holds the length & pointer values without modification or disposal. It's only the subclasses that handle creation and disposal.

--- YARG.Core/IO/Disposables/FixedArray.cs
+++ YARG.Core/IO/Disposables/FixedArray.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using YARG.Core.Logging;
 
-namespace YARG.Core.IO
+namespace YARG.Core.IO.Disposables
 {
     /// <summary>
     /// A wrapper interface over a fixed area of unmanaged memory.
@@ -21,9 +22,14 @@ namespace YARG.Core.IO
     /// <typeparam name="T">The unmanaged type contained in the block of memory</typeparam>
     [DebuggerDisplay("Length = {Length}")]
     [DebuggerTypeProxy(typeof(FixedArray<>.FixedArrayDebugView))]
-    public sealed unsafe class FixedArray<T> : IEnumerable<T>, IDisposable
+    public unsafe class FixedArray<T> : IDisposable
         where T : unmanaged
     {
+        public static FixedArray<T> Alias(T* ptr, long length)
+        {
+            return new FixedArray<T>(ptr, length);
+        }
+
         /// <summary>
         /// Pointer to the beginning of the memory block.<br></br>
         /// DO NOT TOUCH UNLESS YOU ENSURE YOU'RE WITHIN BOUNDS
@@ -33,109 +39,36 @@ namespace YARG.Core.IO
         /// <summary>
         /// Number of elements within the block
         /// </summary>
-        public readonly int Length;
+        public readonly long Length;
 
-        private bool _disposedValue;
-        private readonly GCHandle _handle;
-
-        /// <summary>
-        /// Fully loads the data of a file into a fixed location in memory
-        /// </summary>
-        /// <remarks>Be wary of any file I/O related exceptions. Those will NOT be handled</remarks>
-        /// <param name="path">The file to grab</param>
-        /// <returns>A new FixedArray filled with the file's data</returns>
-        public static FixedArray<T> Load(string path)
-        {
-            using var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 1);
-            return Load(fs);
-        }
-
-        /// <summary>
-        /// Fully loads the data of a stream (or whatever is leftover in the stream)
-        /// into a fixed location in memory
-        /// </summary>
-        /// <remarks>Any exceptions thrown from attempting to read the stream will NOT be handled</remarks>
-        /// <param name="stream">The stream to read from</param>
-        /// <returns>A new FixedArray filled with the stream's leftover data</returns>
-        public static FixedArray<T> Load(Stream stream)
-        {
-            int length = (int)(stream.Length - stream.Position);
-            return Load(stream, length);
-        }
-
-        /// <summary>
-        /// Loads the given amount of data from a stream at its current position
-        /// into a fixed location in memory
-        /// </summary>
-        /// <remarks>Any exceptions thrown from attempting to read the stream will NOT be handled</remarks>
-        /// <param name="stream">The stream to read from</param>
-        /// <param name="length">The amount of data (in bytes) to read</param>
-        /// <returns>A new FixedArray filled with the requested amount of data</returns>
-        public static FixedArray<T> Load(Stream stream, int length)
-        {
-            if (stream.Position + length > stream.Length)
-                throw new EndOfStreamException();
-
-            byte* buffer = (byte*) Marshal.AllocHGlobal(length);
-            var array = new FixedArray<T>((T*)buffer, length / sizeof(T));
-
-            // Use counter on the chance an exception occurs during read
-            using var counter = DisposableCounter.Wrap(array);
-            stream.Read(new Span<byte>(buffer, length));
-            return counter.Release();
-        }
-
-        /// <summary>
-        /// Allocates an uninitialized block of memory
-        /// </summary>
-        /// <param name="length">The number of elements (of `T`) to allocate for</param>
-        /// <returns>A new FixedArray with the requested amount of allocated memory</returns>
-        /// <exception cref="OutOfMemoryException"></exception>
-        public static FixedArray<T> Alloc(int length)
-        {
-            int bufferLength = length * sizeof(T);
-            var ptr = (T*)Marshal.AllocHGlobal(bufferLength);
-            return new FixedArray<T>(ptr, length);
-        }
-
-        /// <summary>
-        /// Pins a managed array to a fixed location
-        /// </summary>
-        /// <remarks>PREFER STACK-ALLOCATED ARRAYS OVER THIS WHEN POSSIBLE</remarks>
-        /// <param name="array">The managed array to pin</param>
-        /// <returns>A FixedArray that points to the location of the now-pinned array</returns>
-        /// <exception cref="OutOfMemoryException"></exception>
-        public static FixedArray<T> Pin(T[] array)
-        {
-            var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
-            return new FixedArray<T>((T*)handle.AddrOfPinnedObject(), array.Length, handle);
-        }
-
-        /// <param name="disposal">The function to use when the object needs to be disposed</param>
-        private FixedArray(T* ptr, int length, GCHandle handle = default)
+        protected FixedArray(T* ptr, long length)
         {
             Ptr = ptr;
             Length = length;
-            _handle = handle;
         }
+
+        /// <summary>
+        /// Provides the pointer to the block of memory in IntPtr form
+        /// </summary>
+        public IntPtr IntPtr => (IntPtr) Ptr;
+
+        /// <summary>
+        /// Provides a ReadOnlySpan over the block of memory
+        /// </summary>
+        public ReadOnlySpan<T> ReadOnlySpan => new(Ptr, (int)Length);
+
+        public UnmanagedMemoryStream ToStream() => new((byte*)Ptr, (int)(Length * sizeof(T)));
 
         /// <summary>
         /// Indexer into the fixed block of memory
         /// </summary>
         /// <param name="index"></param>
-        /// <returns>A reference to the object at the provided index</returns>
-        /// <exception cref="ObjectDisposedException"></exception>
         /// <exception cref="IndexOutOfRangeException"></exception>
-        public ref T this[int index]
+        public ref T this[long index]
         {
             get
             {
-                if (_disposedValue)
-                {
-                    throw new ObjectDisposedException(GetType().Name);
-                }
-
-                if (index < 0 || Length <= index )
+                if (index < 0 || Length <= index)
                 {
                     throw new IndexOutOfRangeException();
                 }
@@ -143,114 +76,16 @@ namespace YARG.Core.IO
             }
         }
 
-        /// <summary>
-        /// Creates a span over the requested slice of memory
-        /// </summary>
-        /// <param name="offset">Starting position of the slice</param>
-        /// <param name="count">Number of elements in the slice</param>
-        /// <returns></returns>
-        /// <exception cref="ObjectDisposedException"></exception>
-        /// <exception cref="IndexOutOfRangeException"></exception>
-        public Span<T> Slice(int offset, int count)
+        public virtual void Dispose()
         {
-            if (_disposedValue)
-            {
-                throw new ObjectDisposedException(GetType().Name);
-            }
-
-            if (0 <= offset && offset + count <= Length)
-                return new Span<T>(Ptr + offset, count);
-            throw new IndexOutOfRangeException();
-        }
-
-        /// <summary>
-        /// Provides the pointer to the block of memory in IntPtr form
-        /// </summary>
-        /// <exception cref="ObjectDisposedException"></exception>
-        public IntPtr IntPtr
-        {
-            get
-            {
-                if (_disposedValue)
-                {
-                    throw new ObjectDisposedException(GetType().Name);
-                }
-                return (IntPtr)Ptr;
-            }
-        }
-
-        /// <summary>
-        /// Provides a Span over the block of memory
-        /// </summary>
-        /// <exception cref="ObjectDisposedException"></exception>
-        public Span<T> Span
-        {
-            get
-            {
-                if (_disposedValue)
-                {
-                    throw new ObjectDisposedException(GetType().Name);
-                }
-                return new Span<T>(Ptr, Length);
-            }
-        }
-
-        /// <summary>
-        /// Provides a ReadOnlySpan over the block of memory
-        /// </summary>
-        /// <exception cref="ObjectDisposedException"></exception>
-        public ReadOnlySpan<T> ReadOnlySpan
-        {
-            get
-            {
-                if (_disposedValue)
-                {
-                    throw new ObjectDisposedException(GetType().Name);
-                }
-                return new ReadOnlySpan<T>(Ptr, Length);
-            }
-        }
-
-        /// <summary>
-        /// Provides a copy of the block of memory in a managed array
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="ObjectDisposedException"></exception>
-        public T[] ToArray()
-        {
-            if (_disposedValue)
-            {
-                throw new ObjectDisposedException(GetType().Name);
-            }
-
-            var array = new T[Length];
-            Span.CopyTo(array);
-            return array;
-        }
-
-        private void Dispose(bool disposing)
-        {
-            if (!_disposedValue)
-            {
-                if (!_handle.IsAllocated)
-                {
-                    Marshal.FreeHGlobal(IntPtr);
-                }
-                else
-                {
-                    _handle.Free();
-                }
-                _disposedValue = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        ~FixedArray() { Dispose(false); }
+        ~FixedArray()
+        {
+            YargLogger.LogWarning("Dev warning: only use FixedArray types IF YOU MANUALLY DISPOSE! Not doing so defeats the purpose!");
+            Dispose();
+        }
 
         private sealed class FixedArrayDebugView
         {
@@ -261,46 +96,7 @@ namespace YARG.Core.IO
             }
 
             [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
-            public Span<T> Items => array.Span;
-        }
-
-
-        public IEnumerator GetEnumerator() { return new Enumerator(this); }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            return ((IEnumerable<T>)this).GetEnumerator();
-        }
-
-        public struct Enumerator : IEnumerator<T>, IEnumerator
-        {
-            private readonly FixedArray<T> _arr;
-            private int _index;
-
-            internal Enumerator(FixedArray<T> arr)
-            {
-                _arr = arr;
-                _index = -1;
-            }
-
-            public void Dispose()
-            {
-            }
-
-            public bool MoveNext()
-            {
-                ++_index;
-                return _index < _arr.Length;
-            }
-
-            public readonly T Current => _arr[_index];
-
-            readonly object IEnumerator.Current => Current;
-
-            void IEnumerator.Reset()
-            {
-                _index = -1;
-            }
+            public ReadOnlySpan<T> Items => array.ReadOnlySpan;
         }
     }
 }
