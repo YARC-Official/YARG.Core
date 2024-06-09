b378e8c965a1c7b61fdef67cf264d0abb89af904
sunset-cf <cfische5@gmail.com>
2024-06-08 19:46:44 -0700

    Moved velocity logic from DrumNote to DrumsEngine.
    Changed DrumNote.HitVelocity and DrumsEngineState.HitVelocity to nullable floats

--- YARG.Core/Engine/Drums/DrumsEngineState.cs
+++ YARG.Core/Engine/Drums/DrumsEngineState.cs
@@ -9,14 +9,14 @@ namespace YARG.Core.Engine.Drums
         /// be based off of <see cref="FourLaneDrumPad"/> or <see cref="FiveLaneDrumPad"/>.
         /// </summary>
         public int? PadHit;
-        public float HitVelocity = 1;
+        public float? HitVelocity;
 
         public override void Reset()
         {
             base.Reset();
 
             PadHit = null;
-            HitVelocity = 1;
+            HitVelocity = null;
         }
     }
 }
