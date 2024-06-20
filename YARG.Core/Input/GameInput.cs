b4625a415746d7d8e018ef8c066e9485a13cdea0
Nathan <nathanhurst03@gmail.com>
2024-06-19 23:56:52 -0600

    Adjust how GameInput memory layout is handled

--- YARG.Core/Input/GameInput.cs
+++ YARG.Core/Input/GameInput.cs
@@ -1,41 +1,45 @@
-﻿using System;
+using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using YARG.Core.Extensions;
 
 namespace YARG.Core.Input
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public readonly struct GameInput
     {
-        public double Time { get; }
-        public int    Action { get; }
+        [FieldOffset(0)]
+        public readonly double Time;
+        [FieldOffset(8)]
+        public readonly int Action;
 
-        private readonly int _value;
-
-        public int   Integer => _value;
-        public float Axis => GetValue<float>(_value);
-        public bool  Button => GetValue<bool>(_value);
+        // Union emulation, each of these fields will share the same memory
+        [FieldOffset(12)]
+        public readonly int Integer;
+        [FieldOffset(12)]
+        public readonly float Axis;
+        [FieldOffset(12)]
+        public readonly bool Button;
 
         public GameInput(double time, int action, int value) : this()
         {
             Time = time;
             Action = action;
-            _value = SetValue(value);
+            Integer = value;
         }
 
         public GameInput(double time, int action, float value) : this()
         {
             Time = time;
             Action = action;
-            _value = SetValue(value);
+            Axis = value;
         }
 
         public GameInput(double time, int action, bool value) : this()
         {
             Time = time;
             Action = action;
-            _value = SetValue(value);
+            Button = value;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -64,34 +68,6 @@ namespace YARG.Core.Input
             where TAction : unmanaged, Enum
         {
             return Action.Convert<TAction>();
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static T GetValue<T>(int value)
-            where T : unmanaged
-        {
-            byte bValue = (byte)value;
-            short sValue = (short)value;
-            return Unsafe.SizeOf<T>() switch
-            {
-                sizeof(byte) => Unsafe.As<byte, T>(ref bValue),
-                sizeof(short) => Unsafe.As<short, T>(ref sValue),
-                sizeof(int) => Unsafe.As<int, T>(ref value),
-                _ => throw new ArgumentException($"Cannot convert to {typeof(T).Name} from an int!")
-            };
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int SetValue<T>(T value)
-            where T : unmanaged
-        {
-            return Unsafe.SizeOf<T>() switch
-            {
-                sizeof(byte) => Unsafe.As<T, byte>(ref value),
-                sizeof(short) => Unsafe.As<T, short>(ref value),
-                sizeof(int) => Unsafe.As<T, int>(ref value),
-                _ => throw new ArgumentException($"Cannot convert from {typeof(T).Name} to an int!")
-            };
         }
     }
 }
