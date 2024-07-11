416767224b66bdd1e6b03e36d3dd5d68e7e46584
Nathan <nathanhurst03@gmail.com>
2024-07-11 00:45:15 -0600

    Add helper for song loading/caching to test console

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+using System;
+using YARG.Core.Chart;
 using YARG.Core.Logging;
+using YARG.TestConsole;
+
+#pragma warning disable CS8321 // Local function is declared but never used
 
 // Temporary console app for quick and dirty testing
 // Changes to this generally shouldn't be committed, but common test procedures are fine to keep around
@@ -10,3 +14,21 @@ Console.WriteLine();
 Console.WriteLine("Press any key to continue...");
 YargLogger.KillLogger();
 Console.ReadKey(intercept: true);
+
+static SongChart LoadIniChart(string directory)
+{
+    return CacheLoader.LoadIni(CacheLoader.LoadCache(), directory)
+        .LoadChart() ?? throw new Exception("Could not load chart!");
+}
+
+static SongChart LoadSngChart(string filePath)
+{
+    return CacheLoader.LoadSng(CacheLoader.LoadCache(), filePath)
+        .LoadChart() ?? throw new Exception("Could not load chart!");
+}
+
+static SongChart LoadCONChart(string songId)
+{
+    return CacheLoader.LoadCON(CacheLoader.LoadCache(), songId)
+        .LoadChart() ?? throw new Exception("Could not load chart!");
+}
