2d6068849c6cbc383ac47953903ceae5165ee09a
Nathan <nathanhurst03@gmail.com>
2024-08-18 05:04:33 -0600

    Revert removal of ChartEvent.Equals I presume this was accidental; it broke the chart parsing unit tests lol

--- YARG.Core/Chart/Events/ChartEvent.cs
+++ YARG.Core/Chart/Events/ChartEvent.cs
@@ -13,6 +13,9 @@ namespace YARG.Core.Chart
         public uint TickLength { get; set; }
         public uint TickEnd    => Tick + TickLength;
 
+        // For subclasses that set the base properties through other parameters
+        public ChartEvent() {}
+
         public ChartEvent(double time, double timeLength, uint tick, uint tickLength)
         {
             Time = time;
@@ -25,9 +28,13 @@ namespace YARG.Core.Chart
             : this(other.Time, other.TimeLength, other.Tick, other.TickLength)
         {
         }
-        public ChartEvent()
+
+        protected bool Equals(ChartEvent other)
         {
-            // For subclasses that set the base properties through other parameters
+            return Time == other.Time &&
+                TimeLength == other.TimeLength &&
+                Tick == other.Tick &&
+                TickLength == other.TickLength;
         }
     }
 }
