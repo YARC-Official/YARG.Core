98de8ca0204f23e5fae9060e7cbcbcd2cb912a28
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-09-04 20:28:00 -0400

    Inherit from `BaseEngine`

--- YARG.Core/Engine/ProGuitar/Engines/YargProGuitarEngine.cs
+++ YARG.Core/Engine/ProGuitar/Engines/YargProGuitarEngine.cs
@@ -1,7 +1,13 @@
-﻿namespace YARG.Core.Engine.ProGuitar.Engines
+﻿using YARG.Core.Chart;
+
+namespace YARG.Core.Engine.ProGuitar.Engines
 {
     public class YargProGuitarEngine : ProGuitarEngine
     {
-
+        public YargProGuitarEngine(InstrumentDifficulty<ProGuitarNote> chart, SyncTrack syncTrack,
+            ProGuitarEngineParameters engineParameters, bool isBot)
+            : base(chart, syncTrack, engineParameters, isBot)
+        {
+        }
     }
 }
--- YARG.Core/Engine/ProGuitar/ProGuitarEngine.cs
+++ YARG.Core/Engine/ProGuitar/ProGuitarEngine.cs
@@ -1,7 +1,46 @@
-﻿namespace YARG.Core.Engine.ProGuitar
+﻿using YARG.Core.Chart;
+using YARG.Core.Input;
+
+namespace YARG.Core.Engine.ProGuitar
 {
-    public class ProGuitarEngine
+    public class ProGuitarEngine : BaseEngine<ProGuitarNote, ProGuitarEngineParameters,
+        ProGuitarStats>
     {
-        
+        public ProGuitarEngine(InstrumentDifficulty<ProGuitarNote> chart, SyncTrack syncTrack,
+            ProGuitarEngineParameters engineParameters, bool isBot)
+            : base(chart, syncTrack, engineParameters, false, isBot)
+        {
+        }
+
+        protected override void UpdateBot(double time)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void MutateStateWithInput(GameInput gameInput)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateHitLogic(double time)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void CheckForNoteHit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool CanNoteBeHit(ProGuitarNote note) => throw new System.NotImplementedException();
+
+        protected override bool CanSustainHold(ProGuitarNote note) => throw new System.NotImplementedException();
+
+        protected override void AddScore(ProGuitarNote note)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override int CalculateBaseScore() => throw new System.NotImplementedException();
     }
 }
