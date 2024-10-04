afa54ab8b3908d26f69b71369e018403ec2ff2a3
sonicfind <sonicfind2@gmail.com>
2024-10-04 18:34:27 -0500

    ParseSettings and Moonscraper cleanup
    Since the scanning process now pre-calculates HopoThreshold and SustainCutoffThreshold, we need to adjust the reader code so that it doesn't re-calculate the thresholds to incorrect values when called from a song entry.
    
    + Remove `HopoFreq_FoF` & `EighthNoteHopo` ParseSettings variables
    + Remove `GetHopoThreshold`-like functions
    + Convert MoonSong resolution to uint

--- YARG.Core/Chart/ParsingProperties.cs
+++ YARG.Core/Chart/ParsingProperties.cs
@@ -73,23 +73,6 @@ namespace YARG.Core.Chart
         public long HopoThreshold;
 
         /// <summary>
-        /// The FoF HOPO threshold setting number to use.
-        /// </summary>
-        /// <remarks>
-        /// Uses the <c>hopofreq</c> tag from song.ini files.<br/>
-        /// 0 -> 1/24th note, 1 -> 1/16th note, 2 -> 1/12th note, 3 -> 1/8th note, 4 -> 1/6th note, 5 -> 1/4th note.
-        /// </remarks>
-        public int HopoFreq_FoF;
-
-        /// <summary>
-        /// Set the HOPO threshold to a 1/8th note instead of a 1/12th note.
-        /// </summary>
-        /// <remarks>
-        /// Uses the <c>eighthnote_hopo</c> tag from song.ini files.
-        /// </remarks>
-        public bool EighthNoteHopo;
-
-        /// <summary>
         /// Skip marking single notes after chords as HOPOs
         /// if the single note shares a fret with the chord.
         /// </summary>
@@ -120,43 +103,5 @@ namespace YARG.Core.Chart
         /// Defaults to 116.
         /// </remarks>
         public int StarPowerNote;
-
-        /// <summary>
-        /// Calculates the HOPO threshold to use from the various HOPO settings.
-        /// </summary>
-        public readonly float GetHopoThreshold(float resolution)
-        {
-            // Prefer in this order:
-            // 1. hopo_threshold
-            // 2. eighthnote_hopo
-            // 3. hopofreq
-
-            if (HopoThreshold > 0)
-            {
-                return HopoThreshold;
-            }
-            else if (EighthNoteHopo)
-            {
-                return resolution / 2;
-            }
-            else if (HopoFreq_FoF >= 0)
-            {
-                int denominator = HopoFreq_FoF switch
-                {
-                    0 => 24,
-                    1 => 16,
-                    2 => 12,
-                    3 => 8,
-                    4 => 6,
-                    5 => 4,
-                    _ => throw new NotImplementedException($"Unhandled hopofreq value {HopoFreq_FoF}!")
-                };
-                return (resolution * 4) / denominator;
-            }
-            else
-            {
-                return resolution / 3;
-            }
-        }
     }
 }
