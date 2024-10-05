47fdce960f9e344c95ed0a42854c201432d80f6a
sunset-cf <cfische5@gmail.com>
2024-10-05 16:59:32 -0700

    Changed engine countdown events to be purely time-based

--- YARG.Core/Chart/Events/WaitCountdown.cs
+++ YARG.Core/Chart/Events/WaitCountdown.cs
@@ -1,59 +1,18 @@
 using System.Collections.Generic;
+using YARG.Core.Logging;
 
 namespace YARG.Core.Chart
 {
     public class WaitCountdown : ChartEvent
     {
         public const float MIN_SECONDS = 9;
-        public const uint MIN_MEASURES = 4;
-        public const float MIN_MEASURE_LENGTH = 1;
-        public const float FADE_ANIM_LENGTH = 0.45f;
-        public const int END_COUNTDOWN_MEASURE = 1;
-
-        public int TotalMeasures => _measureBeatlines.Count;
+        public const float END_COUNTDOWN_SECOND = 1f;
 
         //The time where the countdown should start fading out and overstrums will break combo again
-        public double DeactivateTime => _measureBeatlines[^(END_COUNTDOWN_MEASURE + 1)].Time;
-        public bool IsActive => MeasuresLeft > END_COUNTDOWN_MEASURE;
+        public double DeactivateTime => TimeEnd - END_COUNTDOWN_SECOND;
 
-        private List<Beatline> _measureBeatlines;
-
-        public int MeasuresLeft {get; private set; }
-
-        public WaitCountdown(List<Beatline> measureBeatlines)
+        public WaitCountdown(double time, double timeLength, uint tick, uint tickLength) : base(time, timeLength, tick, tickLength)
         {
-            _measureBeatlines = measureBeatlines;
-
-            var firstCountdownMeasure = measureBeatlines[0];
-            var lastCountdownMeasure = measureBeatlines[^1];
-
-            Time = firstCountdownMeasure.Time;
-            Tick = firstCountdownMeasure.Tick;
-            TimeLength = lastCountdownMeasure.Time - Time;
-            TickLength = lastCountdownMeasure.Tick - Tick;
-
-            MeasuresLeft = TotalMeasures;
-        }
-
-        public int CalculateMeasuresLeft(uint currentTick)
-        {
-            int newMeasuresLeft;
-            if (currentTick >= TickEnd)
-            {
-                newMeasuresLeft = 0;
-            }
-            else if (currentTick < Tick)
-            {
-                newMeasuresLeft = TotalMeasures;
-            }
-            else
-            {
-                newMeasuresLeft = TotalMeasures - _measureBeatlines.GetIndexOfNext(currentTick);
-            }
-
-            MeasuresLeft = newMeasuresLeft;
-
-            return newMeasuresLeft;
         }
     }
 }
