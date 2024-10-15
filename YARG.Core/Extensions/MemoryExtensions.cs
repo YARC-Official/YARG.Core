d64bf85e5e7657817fdcd7f214c0af25d1f5f7eb
Nathan <nathanhurst03@gmail.com>
2024-10-14 20:03:31 -0600

    Add hex string format wrapper type for byte arrays

--- YARG.Core/Extensions/MemoryExtensions.cs
+++ YARG.Core/Extensions/MemoryExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using YARG.Core.Logging;
 
 namespace YARG.Core.Extensions
 {
@@ -50,50 +51,89 @@ namespace YARG.Core.Extensions
         }
 
         public static string ToHexString(this byte[] buffer, bool dashes = true)
-            => ToHexString(buffer.AsSpan(), dashes);
+            => buffer.AsSpan().ToHexString(dashes);
+
+        public static string ToHexString(this Memory<byte> buffer, bool dashes = true)
+            => ToHexString((ReadOnlySpan<byte>)buffer.Span, dashes);
+
+        public static string ToHexString(this Span<byte> buffer, bool dashes = true)
+            => ToHexString((ReadOnlySpan<byte>)buffer, dashes);
 
         public static string ToHexString(this ReadOnlyMemory<byte> buffer, bool dashes = true)
             => ToHexString(buffer.Span, dashes);
 
         public static string ToHexString(this ReadOnlySpan<byte> buffer, bool dashes = true)
         {
+            Span<char> chars = stackalloc char[(buffer.Length * 3) - 1];
+            bool success = TryFormatHex(buffer, chars, out _, dashes);
+            YargLogger.Assert(success, "Failed to format bytes as hex string!");
+            return new string(chars);
+        }
+
+        public static bool TryFormatHex(this byte[] buffer, Span<char> destination,
+            out int charsWritten, bool dashes = true)
+            => TryFormatHex(buffer.AsSpan(), destination, out charsWritten, dashes);
+
+        public static bool TryFormatHex(this Memory<byte> buffer, Span<char> destination,
+            out int charsWritten, bool dashes = true)
+            => TryFormatHex((ReadOnlySpan<byte>)buffer.Span, destination, out charsWritten, dashes);
+
+        public static bool TryFormatHex(this Span<byte> buffer, Span<char> destination,
+            out int charsWritten, bool dashes = true)
+            => TryFormatHex((ReadOnlySpan<byte>)buffer, destination, out charsWritten, dashes);
+
+        public static bool TryFormatHex(this ReadOnlyMemory<byte> buffer, Span<char> destination,
+            out int charsWritten, bool dashes = true)
+            => TryFormatHex(buffer.Span, destination, out charsWritten, dashes);
+
+        public static bool TryFormatHex(this ReadOnlySpan<byte> buffer, Span<char> destination,
+            out int charsWritten, bool dashes = true)
+        {
             const string characters = "0123456789ABCDEF";
 
             if (buffer.IsEmpty)
-                return "";
+            {
+                charsWritten = 0;
+                return true;
+            }
+
+            int charsNeeded = (buffer.Length * 3) - 1;
+            if (destination.Length < charsNeeded)
+            {
+                // Hint as to how many characters are needed
+                charsWritten = charsNeeded;
+                return false;
+            }
+
+            static void WriteByte(Span<char> destination, byte value, ref int index, ref int charsWritten)
+            {
+                destination[index++] = characters[(value & 0xF0) >> 4];
+                destination[index++] = characters[value & 0x0F];
+                charsWritten += 2;
+            }
+
+            // Write first byte directly
+            int destIndex = 0;
+            charsWritten = 0;
+            WriteByte(destination, buffer[0], ref destIndex, ref charsWritten);
 
             if (dashes)
             {
-                const int charsPerByte = 3;
-                Span<char> stringBuffer = stackalloc char[buffer.Length * charsPerByte];
-                for (int i = 0; i < buffer.Length; i++)
+                for (int i = 1; i < buffer.Length; i++)
                 {
-                    byte value = buffer[i];
-                    int stringIndex = i * charsPerByte;
-                    stringBuffer[stringIndex] = characters[(value & 0xF0) >> 4];
-                    stringBuffer[stringIndex + 1] = characters[value & 0x0F];
-                    stringBuffer[stringIndex + 2] = '-';
+                    WriteByte(destination, buffer[i], ref destIndex, ref charsWritten);
+                    destination[destIndex++] = '-';
                 }
-
-                // Exclude last '-'
-                stringBuffer = stringBuffer[..^1];
-
-                return new string(stringBuffer);
             }
             else
             {
-                const int charsPerByte = 2;
-                Span<char> stringBuffer = stackalloc char[buffer.Length * charsPerByte];
-                for (int i = 0; i < buffer.Length; i++)
+                for (int i = 1; i < buffer.Length; i++)
                 {
-                    byte value = buffer[i];
-                    int stringIndex = i * charsPerByte;
-                    stringBuffer[stringIndex] = characters[(value & 0xF0) >> 4];
-                    stringBuffer[stringIndex + 1] = characters[value & 0x0F];
+                    WriteByte(destination, buffer[i], ref destIndex, ref charsWritten);
                 }
-
-                return new string(stringBuffer);
             }
+
+            return true;
         }
     }
 }
