1e3e38305586a65b9dca30f1c6ccb5b4d23b5d59
Thome Valentin <thomeval@hotmail.com>
2024-10-18 12:37:14 +0200

    Added "No Groove" default highway preset Make "Basic" preset even more basic

--- YARG.Core/Game/Presets/HighwayPreset.Defaults.cs
+++ YARG.Core/Game/Presets/HighwayPreset.Defaults.cs
@@ -9,8 +9,15 @@ namespace YARG.Core.Game
         public static HighwayPreset Basic = new("Basic", true)
         {
             StarPowerColor = Color.FromArgb(0, 0, 0, 0),
-            BackgroundGroovePatternColor = Color.FromArgb(0,0,0,0),
             BackgroundPatternColor = Color.FromArgb(0, 0, 0, 0),
+            BackgroundBaseColor2 = Color.FromArgb(0, 0, 0, 0),
+            BackgroundGroovePatternColor = Color.FromArgb(0,0,0,0),
+            BackgroundGrooveBaseColor1 = Color.FromArgb(255, 15, 15, 15),
+            BackgroundGrooveBaseColor2 = Color.FromArgb(0, 0, 0, 0)
+        };
+        public static HighwayPreset NoGroove = new("No Groove", true)
+        {
+            BackgroundGroovePatternColor = Color.FromArgb(255, 87, 87, 87),
             BackgroundGrooveBaseColor1 = Color.FromArgb(255, 15, 15, 15),
             BackgroundGrooveBaseColor2 = Color.FromArgb(38, 75, 75, 75)
         };
@@ -19,6 +26,7 @@ namespace YARG.Core.Game
         {
             Default,
             Basic,
+            NoGroove
         };
     }
 }
