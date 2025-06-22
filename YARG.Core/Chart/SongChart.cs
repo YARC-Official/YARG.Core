4978a94df673a13d3969334f9b21b6410c79e7c1
wyrdough <nathan@nwacg.net>
2025-06-22 17:06:15 -0400

    generate beatlines for 5 seconds past the end of the song
    - This is probably too simple, but the point of it is to keep the 5GS meter flashing all the way until we go to the score screen

--- YARG.Core/Chart/SongChart.cs
+++ YARG.Core/Chart/SongChart.cs
@@ -148,10 +148,11 @@ namespace YARG.Core.Chart
             // Dj = loader.LoadDjTrack(Instrument.Dj);
 
             // Ensure beatlines are present
-            if (SyncTrack.Beatlines is null or { Count: < 1 })
-            {
-                SyncTrack.GenerateBeatlines(GetLastTick());
-            }
+            // if (SyncTrack.Beatlines is null or { Count: < 1 })
+            // {
+            //     SyncTrack.GenerateBeatlines(GetLastTick());
+            // }
+            SyncTrack.GenerateBeatlines(GetEndTime() + 5.0);
 
             // Use beatlines to place auto-generated drum activation phrases for charts without manually authored phrases
             CreateDrumActivationPhrases();
