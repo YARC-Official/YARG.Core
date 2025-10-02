1da7a4c1e558912db5957769dc0982d8e75b3433
Thome Valentin <thomeval@hotmail.com>
2025-10-02 13:33:09 +0200

    Implement support for Band Score Multipliers into YARG.Core

---
+++ YARG.Core/Engine/EngineManager.Band.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YARG.Core.Engine
+{
+    public partial class EngineManager
+    {
+        public int BandMultiplier => Math.Max(_starpowerCount * 2, 1);
+
+        private void UpdateBandMultiplier()
+        {
+            foreach (var engine in Engines)
+            {
+                engine.Engine.UpdateBandMultiplier(BandMultiplier);
+            }
+        }
+    }
+}
