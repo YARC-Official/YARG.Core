a2ad1b45aae549dee325eec0af4a20f5d3a936a1
Anton Romanov <theli.ua@gmail.com>
2025-12-26 13:02:34 -0800

    Make TimeInStarPower frame independent
    Current implementation suffers from following problems:
     * TimeToMeasureTick and MeasureTickToTime are not mutual inverses due to rounding used.
        Therefore When MeasureTickToTime is used to enqueue an even to terminate SP it may
        actually enqueue it a tick earlier than desired, and the actual SP termination will
        happen on a later (frame dependent) timestamp.
     * Even when we enqueue a time that rounds up to a correct tick - it can still be
        preceded by an even with a smaller timestamp that would also round to the same tick.
        Ending up terminating SP at a correct tick but an inconsistent (frame dependent) time.
    
    Fix here uses binary search to find a smallest(earliest) time that would equal to the desired tick
    that would drain SP to zero for the sp end event. This way that event is always the one that is
    actually terminating SP making it consistent.
    
    I've also tested it by running analysis of the same replay for ~1000 iterations and getting the
    exact same output each time. While before the fix TimeInStarPower would fluctuate slightly.

--- YARG.Core/Engine/BaseEngine.cs
+++ YARG.Core/Engine/BaseEngine.cs
@@ -491,7 +491,7 @@ namespace YARG.Core.Engine
             double lastEndTime = StarPowerEndTime;
 
             StarPowerTickEndPosition = StarPowerTickPosition + BaseStats.StarPowerTickAmount;
-            StarPowerEndTime = SyncTrack.MeasureTickToTime(StarPowerTickEndPosition);
+            StarPowerEndTime = SyncTrack.FindMinTimeForMeasureTick(StarPowerTickEndPosition);
 
             YargLogger.LogFormatTrace(
                 "Updated Star Power end from {0} ({1}) to {2} ({3})",
