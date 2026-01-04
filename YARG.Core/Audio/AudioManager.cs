9bca3e7f4996dbf77377e1030628e9506a345102
Scott Dawson <scott@daw.sn>
2026-01-04 12:53:31 +1100

    Fix issue with non-default device and alternate channels on open

--- YARG.Core/Audio/AudioManager.cs
+++ YARG.Core/Audio/AudioManager.cs
@@ -12,9 +12,9 @@ namespace YARG.Core.Audio
         private bool _disposed;
         private List<StemMixer> _activeMixers = new();
 
-        protected internal readonly SampleChannel[]     SfxSamples     = new SampleChannel[AudioHelpers.SfxSamples.Count];
-        protected internal readonly DrumSampleChannel[] DrumSfxSamples = new DrumSampleChannel[AudioHelpers.DrumSamples.Count];
-        protected internal readonly VoxSampleChannel[]  VoxSamples     = new VoxSampleChannel[AudioHelpers.VoxSamples.Count];
+        protected internal SampleChannel[]     SfxSamples     = new SampleChannel[AudioHelpers.SfxSamples.Count];
+        protected internal DrumSampleChannel[] DrumSfxSamples = new DrumSampleChannel[AudioHelpers.DrumSamples.Count];
+        protected internal VoxSampleChannel[]  VoxSamples     = new VoxSampleChannel[AudioHelpers.VoxSamples.Count];
         protected internal int PlaybackLatency;
         protected internal int MinimumBufferLength;
         protected internal int MaximumBufferLength;
