19334b524aa5b624df3ed5eea778e64807e9921e
wyrdough <nathan@nwacg.net>
2026-02-07 19:06:16 -0500

    Add PauseInfo to ReplayInfo and serialize/deserialize it
    * Updated replay version to 11, includes compatibility with version 10 replays

---
+++ YARG.Core/Replays/PauseInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using YARG.Core.Extensions;
+using YARG.Core.IO;
+
+namespace YARG.Core.Replays
+{
+    public struct PauseInfo
+    {
+        public double PauseTime;
+        public double PauseLength;
+
+        public void Serialize(BinaryWriter stream)
+        {
+            stream.Write(PauseTime);
+            stream.Write(PauseLength);
+        }
+
+        public PauseInfo(ref FixedArrayStream stream, int version)
+        {
+            PauseTime = stream.Read<double>(Endianness.Little);
+            PauseLength = stream.Read<double>(Endianness.Little);
+        }
+    }
+}
