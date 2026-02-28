590ff1f5f3d8cdb44466928a49e66ba6f119b16b
Anton Romanov <theli.ua@gmail.com>
2026-02-27 18:48:47 -0800

    Remove exclamation mark from lyrics when lipsyncing

--- YARG.Core/Chart/LipsyncGenerator.cs
+++ YARG.Core/Chart/LipsyncGenerator.cs
@@ -225,7 +225,7 @@ namespace YARG.Core.Chart
                 return syllable;
 
             var clean = text.ToLowerInvariant()
-                .Replace("-", "").Replace("=", "").Replace("#", "")
+                .Replace("-", "").Replace("=", "").Replace("#", "").Replace("!", "")
                 .Replace("^", "").Replace("$", "").Trim();
 
             if (clean.Length == 0)
