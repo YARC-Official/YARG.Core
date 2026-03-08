1904edf530c7371a22cc2b4ed8e13150d1d8e564
polson <302882+polson@users.noreply.github.com>
2026-03-08 15:47:05 -0500

    Reduce global audio state (#339)
    * Set volume on mixer directly
    
    * Set reverb and whammy pitch directly on mixer
    
    * Move mixer stem handling out of GlobalAudioHandler
    
    * Remove whammy pitch and reverb from StemSettings
    
    * Handle duration when setting volume

--- YARG.Core/Song/Entries/RBCON/SongEntry.RBCON.cs
+++ YARG.Core/Song/Entries/RBCON/SongEntry.RBCON.cs
@@ -255,6 +255,7 @@ namespace YARG.Core.Song
                 return null;
             }
             YargLogger.LogFormatInfo("Loaded {0} stems", mixer.Channels.Count);
+            MixerAudioHandler.SetMixer(mixer);
             return mixer;
         }
 
