f9cb120ad3571b6c1c33cc238f0872cab119e87b
wyrdough <nathan@nwacg.net>
2026-03-28 20:01:06 -0400

    Require strum to collect BRE lane score when using standard buttons

--- YARG.Core/Engine/Guitar/Engines/YargFiveFretGuitarEngine.cs
+++ YARG.Core/Engine/Guitar/Engines/YargFiveFretGuitarEngine.cs
@@ -571,7 +571,7 @@ namespace YARG.Core.Engine.Guitar.Engines
             {
                 pressed = EffectiveButtonMask;
             }
-            else
+            else if (IsBot || !StandardButtonHeld)
             {
                 for (int i = 0; i < fretMask.Length; i++)
                 {
