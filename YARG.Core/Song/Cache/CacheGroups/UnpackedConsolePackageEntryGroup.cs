d8c0c4ef2dce0da3cde997cb6b2f922baa1c4ab8
Supersonicboss1 <leodalgado@gmail.com>
2026-04-09 12:42:28 +0100

    Correctly parse DTA subname to find mid file

--- YARG.Core/Song/Cache/CacheGroups/UnpackedConsolePackageEntryGroup.cs
+++ YARG.Core/Song/Cache/CacheGroups/UnpackedConsolePackageEntryGroup.cs
@@ -14,19 +14,25 @@ namespace YARG.Core.Song.Cache
         {
             try
             {
-                // Peek at the DTA just to detect platform, don't hold onto the data
                 using var data = FixedArray.LoadFile(dtaInfo.FullName);
                 var container = YARGDTAReader.Create(data);
                 while (YARGDTAReader.StartNode(ref container))
                 {
                     string name = YARGDTAReader.GetNameOfNode(ref container, true);
-
-
-                    if (File.Exists(Path.Combine(directory, name, name + ".mid.edat")))
+                    // This is an expensive operation, but happens only once per CON entry group, and is required to parse location
+                    var entry = DTAEntry.Create(name, container);
+                    if (entry.Location is null)
+                    {
+                        YargLogger.LogFormatWarning("Node {0} could not be parsed as a DTA entry, cannot determine entry group, checking next node if available", name);
+                        YARGDTAReader.EndNode(ref container);
+                        continue;
+                    }
+                    string subname = entry.Location[6..entry.Location.IndexOf('/', 6)];
+                    if (File.Exists(Path.Combine(directory, subname, subname + ".mid.edat")))
                     {
                         return UnpackedPKGEntryGroup.Create(directory, dtaInfo, defaultPlaylist, out group);
                     }
-                    if (File.Exists(Path.Combine(directory, name, name + ".mid")))
+                    if (File.Exists(Path.Combine(directory, subname, subname + ".mid")))
                     {
                         return UnpackedCONEntryGroup.Create(directory, dtaInfo, defaultPlaylist, out group);
                     }
