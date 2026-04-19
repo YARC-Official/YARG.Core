a5cd7f85971073468e96f41280a92e83b8cbd968
Raymond Wright <gms@gmscorner.dev>
2026-04-19 18:56:57 +0100

    ﻿Fix EndTickAlmostEquals
    Before the fix two phrases could combine into a unison, even if their
    ends are far apart. This happened in the GH2 rip of Misirlou, where the
    first phrases on guitar and bass formed a unison despite their ends
    being over a beat apart.

--- YARG.Core/Engine/EngineManager.UnisonEvent.cs
+++ YARG.Core/Engine/EngineManager.UnisonEvent.cs
@@ -135,7 +135,7 @@ namespace YARG.Core.Engine
 
             public bool EndTickAlmostEquals(StarPowerSection other, uint tolerance)
             {
-                return TickEnd - other.TickEnd <= tolerance || other.Tick - Tick <= tolerance;
+                return TickEnd - other.TickEnd <= tolerance || other.TickEnd - TickEnd <= tolerance;
             }
         }
 
