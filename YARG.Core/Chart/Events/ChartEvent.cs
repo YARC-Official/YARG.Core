fd6b700f58dac6a790f41b7827d58807ff8cac5a
polson <302882+polson@users.noreply.github.com>
2026-04-27 16:59:03 -0500

    Support for reducing flashing lights

--- YARG.Core/Chart/Events/ChartEvent.cs
+++ YARG.Core/Chart/Events/ChartEvent.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace YARG.Core.Chart
 {
     /// <summary>
@@ -33,5 +36,40 @@ namespace YARG.Core.Chart
                 Tick == other.Tick &&
                 TickLength == other.TickLength;
         }
+
+        /// <summary>
+        /// Walks the list, comparing each event against the last kept event.
+        /// Skips events where shouldSkip returns true.
+        /// </summary>
+        public static List<T> FilterByPrevious<T>(List<T> events, Func<T, T, bool> shouldSkip) where T : ChartEvent
+        {
+            if (events.Count == 0)
+            {
+                return events;
+            }
+
+            var filtered = new List<T>(events.Count) { events[0] };
+            for (var i = 1; i < events.Count; i++)
+            {
+                if (!shouldSkip(events[i], filtered[^1]))
+                {
+                    filtered.Add(events[i]);
+                }
+            }
+            return filtered;
+        }
+
+        /// <summary>
+        /// Reduces events by enforcing a minimum interval between them,
+        /// and optionally dropping duplicates detected by the given predicate.
+        /// </summary>
+        public static List<T> ReduceByInterval<T>(List<T> events, double interval,
+            Func<T, T, bool> isDuplicate = null) where T : ChartEvent
+        {
+            return FilterByPrevious(events, (curr, prev) =>
+                curr.Time - prev.Time < interval ||
+                (isDuplicate != null && isDuplicate(curr, prev)));
+        }
+
     }
 }
