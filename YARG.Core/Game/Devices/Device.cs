5307204a37ab50d04e2d2af90705ec9c1c992263
Will Frick <willfrickvt@gmail.com>
2026-08-02 10:56:38 -0400

    WIP

---
+++ YARG.Core/Game/Devices/Device.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YARG.Core.Game
+{
+    public abstract class Device
+    {
+        private readonly int VERSION = 0;
+
+        public int Version;
+
+        public Guid Id;
+        public string Name;
+
+        public long InputCalibrationMilliseconds;
+        public double InputCalibrationSeconds
+        {
+            get => InputCalibrationMilliseconds / 1000.0;
+            set => InputCalibrationMilliseconds = (long) (value * 1000);
+        }
+    }
+
+    public class Controller : Device
+    {
+        public string Layout;
+        public string Hash;
+    }
+
+    public class Microphone: Device
+    {
+
+    }
+}
