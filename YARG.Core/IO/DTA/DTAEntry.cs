9d06d11ff6d5743fcd86f14f115961f3b07c1b69
asimard1 <as.simard@outlook.com>
2026-09-05 11:36:15 -0400

    Fix folder sorting with multisong CON packs

--- YARG.Core/IO/DTA/DTAEntry.cs
+++ YARG.Core/IO/DTA/DTAEntry.cs
@@ -22,7 +22,6 @@ namespace YARG.Core.IO
         public TextSpan? CharterKeys;
         public TextSpan? CharterProStrings;
         public string? Source;
-        public TextSpan? Playlist;
         public TextSpan? LoadingPhrase;
         public int? YearAsNumber;
         public int? YearSecondaryAsNumber;
@@ -216,7 +215,7 @@ namespace YARG.Core.IO
                     case "date_released": YearAsNumber = int.Parse(YARGDTAReader.ExtractText(ref container).AsSpan(0, 4)); break;
                     case "album_name": Album = YARGDTAReader.ExtractTextBytes(ref container); break;
                     case "album_track_number": AlbumTrack = YARGDTAReader.ExtractInteger<int>(ref container); break;
-                    case "pack_name": Playlist = YARGDTAReader.ExtractTextBytes(ref container); break;
+                    case "pack_name": /*Playlist = YARGDTAReader.ExtractTextBytes(ref container);*/ break;
                     case "base_points": /*BasePoints = YARGDTAReader.Extract<uint>(ref container);*/ break;
                     case "band_fail_cue": /*BandFailCue = YARGDTAReader.ExtractText(ref container);*/ break;
                     case "drum_bank": DrumBank = YARGDTAReader.ExtractText(ref container); break;
